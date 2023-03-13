84caec6e98bc31207122f1f01da49e3d274ccb83
iamakshat01 <akshat@cypherock.com>
2023-03-13 14:58:20 +0530

    WIP: Linking upodate screens

--- apps/desktop/src/pages/onboarding/index.tsx
+++ apps/desktop/src/pages/onboarding/index.tsx
@@ -15,12 +15,21 @@ import { Support } from "./Support";
 import { SupplyChainAttack } from "./cardAuth/Dialogue/SupplyChainAttack";
 import { AppClose } from "./cardAuth/Dialogue/AppClose";
 import { CardPairing } from "./cardAuth/Dialogue/CardPairing";
-import { Splash } from "./getStarted/Splash" 
+import { Splash } from "./getStarted/Splash";
+import { DeviceConnect } from "./update/DeviceConnect";
+import { AppUpdate } from "./update/AppUpdate";
+import { AppUpdateProgress } from "./update/AppUpdateProgress";
+import { AppUpdateSuccess } from "./update/AppUpdateSuccess";
+import { DeviceUpdate } from "./update/DeviceUpdate";
+import { DeviceUpdateProgress } from "./update/DeviceUpdateProgress";
+import { DeviceUpdateSuccess } from "./update/DeviceUpdateSuccess";
+import { UpdateFailed } from "./update/UpdateFailed";
+import { UpdateFailedAgain } from "./update/UpdateFailedAgain";
 
 export const OnboradingMain = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<Splash />}></Route>
+      {/* <Route path="/" element={<Splash />}></Route>
       <Route path="/information" element={<Information />}></Route>
       <Route path="/usage" element={<Usage />}></Route>
       <Route path="/welcome" element={<Welcome />}></Route>
@@ -36,7 +45,19 @@ export const OnboradingMain = (): JSX.Element => {
       <Route path="/supplyChainAttack" element={<SupplyChainAttack />}></Route>
       <Route path="/contactSupport" element={<Support />}></Route>
       <Route path="/closeApp" element={<AppClose />}></Route>
-      <Route path="/cardPair" element={<CardPairing/>}></Route>
+      <Route path="/cardPair" element={<CardPairing/>}></Route> */}
+
+      {/* Update screens routes */}
+      <Route path="/deviceConnect" element={<DeviceConnect />}></Route>
+      <Route path="/" element={<AppUpdate />}></Route>
+      <Route path="/appUpdateProgress" element={<AppUpdateProgress />}></Route>
+      <Route path="/appUpdateSuccess" element={<AppUpdateSuccess />}></Route>
+      <Route path="/deviceUpdate" element={<DeviceUpdate />}></Route>
+      <Route path="/deviceUpdateProgress" element={<DeviceUpdateProgress />}></Route>
+      <Route path="/deviceUpdateSuccess" element={<DeviceUpdateSuccess />}></Route>
+      <Route path="/updateFailed" element={<UpdateFailed />}></Route>
+      <Route path="/updateFailedAgain" element={<UpdateFailedAgain />}></Route>
+
     </Routes>
   );
 };
