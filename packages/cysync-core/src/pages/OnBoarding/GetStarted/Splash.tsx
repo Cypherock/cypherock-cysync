0a4ae3b2a7e97b3e885554adf3de2a7ae24fc44a
Md Irshad Ansari <irshad@cypherock.com>
2023-05-05 20:16:51 +0530

    build(fix): ts configs

--- packages/cysync-core/src/pages/OnBoarding/GetStarted/Splash.tsx
+++ packages/cysync-core/src/pages/OnBoarding/GetStarted/Splash.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@cypherock/cysync-ui';
 import React, { ReactElement } from 'react';
+import { SDK } from '@cypherock/sdk-core';
 
 export const Splash = (): ReactElement => (
   <>
-    <Button variation="primary">Get Started</Button>
+    <Button onClick={() => console.log(SDK.create)} variation="primary">
+      Get Started
+    </Button>
     <Button variation="secondary">Get Started</Button>
   </>
 );
