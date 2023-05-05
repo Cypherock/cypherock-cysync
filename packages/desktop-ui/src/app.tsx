9e6c36d65779159591ae96e53110bf1db4d4f029
Subin S K <subinsk284@gmail.com>
2023-05-05 15:21:36 +0530

    fix: build error resolved

--- packages/desktop-ui/src/app.tsx
+++ packages/desktop-ui/src/app.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Splash } from '@cypherock/cysync-core';
+import { theme } from '@cypherock/cysync-ui';
 
-const App = () => <Splash />;
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Splash />
+  </ThemeProvider>
+);
 
 export default App;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,2 @@
 export * from './components/atoms/index';
+export * from './themes/theme.styled';
