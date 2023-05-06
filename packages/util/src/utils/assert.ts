65aacb5c497710fa16502ea73774c47e18443bd7
Tejasv Sharma <tejasvonly@gmail.com>
2023-05-06 14:18:39 +0530

    feat: utils

---
+++ packages/util/src/utils/assert.ts
@@ -0,0 +1,16 @@
+export function assert<T>(
+  condition: T,
+  error: string | Error,
+): asserts condition is Exclude<T, null | undefined> {
+  if (
+    condition === null ||
+    condition === undefined ||
+    (condition as any) === false
+  ) {
+    if (typeof error === 'string') {
+      throw new Error(`AssertionError: ${error}`);
+    } else {
+      throw error;
+    }
+  }
+}
