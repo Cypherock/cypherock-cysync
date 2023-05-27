9280927a1c5c785b7a6b4b319c55e7efc3a1e0b7
Md Irshad Ansari <irshad@cypherock.com>
2023-05-27 17:31:59 +0530

    ci: added early and final release

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const semver = require('semver');
 
 const { getReleaseParams, config, execCommand } = require('./helpers');
 
@@ -27,14 +28,58 @@ const CHANNEL_CONFIG = {
   },
 };
 
+const setDesktopAppVersion = async params => {
+  if (config.CHANNEL !== config.RELEASE_CHANNEL) {
+    params.pkgJson.productName = `${params.pkgJson.productName}-${config.channel}`;
+  }
+
+  const pkgJsonVersion = semver.parse(params.pkgJson.version);
+
+  if (!pkgJsonVersion) {
+    throw new Error('Invalid version in package json');
+  }
+
+  if (config.DO_UPDATE_APP_VERSION) {
+    const versionWithoutChannelPostfix = `${params.version.major}.${params.version.minor}.${params.version.patch}-${config.CHANNEL}`;
+    const tagNameWithoutChannelPostfix = `${config.APP_NAME}@${versionWithoutChannelPostfix}`;
+    let version = versionWithoutChannelPostfix;
+
+    const existingTags = await execCommand(
+      `git tag -l "${tagNameWithoutChannelPostfix}*"`,
+    );
+    const channelVersions = existingTags
+      .trim()
+      .split('\n')
+      .filter(e => !!e)
+      .map(tag =>
+        parseInt(tag.replace(`${tagNameWithoutChannelPostfix}.`, ''), 10),
+      )
+      .sort()
+      .reverse();
+
+    if (channelVersions.length === 0) {
+      version += `.0`;
+    } else {
+      version += `.${channelVersions[0] + 1}`;
+    }
+
+    params.pkgJson.version = version;
+  }
+
+  fs.writeFileSync(
+    path.join(params.appPath, 'package.json'),
+    JSON.stringify(params.pkgJson, undefined, 2),
+  );
+};
+
 const setDesktopAppConfig = async params => {
   const configPath = path.join(params.appPath, 'src', 'config.ts');
   const commitHash = await getCommitHash();
 
   const configJson = {
-    ...(CHANNEL_CONFIG[params.channel] ?? CHANNEL_CONFIG.default),
+    ...(CHANNEL_CONFIG[config.CHANNEL] ?? CHANNEL_CONFIG.default),
     BUILD_VERSION: commitHash.slice(0, 7),
-    CHANNEL: params.channel,
+    CHANNEL: config.CHANNEL,
   };
 
   let configStr = '{\n';
@@ -54,6 +99,7 @@ const run = async () => {
   try {
     const params = await getReleaseParams();
 
+    await setDesktopAppVersion(params);
     await setDesktopAppConfig(params);
   } catch (error) {
     console.error(error);
