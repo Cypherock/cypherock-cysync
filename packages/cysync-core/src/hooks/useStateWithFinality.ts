0596d42839e6c66338b07ebc3aabee5129c60d64
Tejasv Sharma <tejasvonly@gmail.com>
2023-05-31 13:06:19 +0530

    refactor: update return type for usestatewithfinality hook

--- packages/cysync-core/src/hooks/useStateWithFinality.ts
+++ packages/cysync-core/src/hooks/useStateWithFinality.ts
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 export function useStateWithFinality<T>(
   initialState: T,
   finalState: T,
-): [T, (val: T | ((prev: T) => T)) => void, boolean] {
+): [T, React.Dispatch<React.SetStateAction<T>>, boolean] {
   const [state, setState] = React.useState<T>(initialState);
   const isFinalState = useMemo(() => state >= finalState, [state]);
   return [state, setState, isFinalState];
