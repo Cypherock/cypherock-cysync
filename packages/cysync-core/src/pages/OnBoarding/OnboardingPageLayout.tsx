044c64a4831746c5125e7dea1c17624f96057a23
Md Irshad Ansari <irshad@cypherock.com>
2023-06-01 16:59:29 +0530

    refactor: onboarding layout

--- packages/cysync-core/src/pages/OnBoarding/OnboardingPageLayout.tsx
+++ packages/cysync-core/src/pages/OnBoarding/OnboardingPageLayout.tsx
@@ -3,10 +3,12 @@ import {
   EmailHeader,
   OnboardingLayout,
   OnboardingLayoutProps,
+  DialogBoxBackgroundFooterProps,
+  DialogBoxBackgroundHeaderProps,
 } from '@cypherock/cysync-ui';
 import React, { ReactNode } from 'react';
 
-import { useAppSelector, selectLanguage } from '~/store';
+import { useAppSelector, selectLanguage, ILangState } from '~/store';
 
 export interface OnboardingPageLayoutProps
   extends Omit<OnboardingLayoutProps, 'headerProps' | 'footerProps'> {
@@ -15,6 +17,48 @@ export interface OnboardingPageLayoutProps
   withEmail?: boolean;
   withBack?: boolean;
 }
+
+const parseHeaderProps = ({
+  withHelp,
+  withEmail,
+  lang,
+}: {
+  withHelp?: boolean;
+  withEmail?: boolean;
+  lang: ILangState;
+}) => {
+  let headerProps: DialogBoxBackgroundHeaderProps | undefined;
+
+  if (withHelp || withEmail) {
+    headerProps = {};
+
+    if (withHelp) {
+      headerProps.topRightComponent = <HelpHeader text={lang.strings.help} />;
+    }
+    if (withEmail) {
+      headerProps.topLeftComponent = <EmailHeader email="user@example.com" />;
+    }
+  }
+
+  return headerProps;
+};
+
+const parseFooterProps = ({
+  withBack,
+  lang,
+}: {
+  withBack?: boolean;
+  lang: ILangState;
+}) => {
+  let footerProps: DialogBoxBackgroundFooterProps | undefined;
+  if (withBack) {
+    footerProps = {
+      backText: lang.strings.back,
+    };
+  }
+
+  return footerProps;
+};
 
 export const OnboardingPageLayout: React.FC<OnboardingPageLayoutProps> = ({
   children,
@@ -34,21 +78,8 @@ export const OnboardingPageLayout: React.FC<OnboardingPageLayoutProps> = ({
       text={text}
       currentState={currentState}
       totalState={totalState}
-      headerProps={{
-        topRightComponent: withHelp ? (
-          <HelpHeader text={lang.strings.help} />
-        ) : undefined,
-        topLeftComponent: withEmail ? (
-          <EmailHeader email="user@example.com" />
-        ) : undefined,
-      }}
-      footerProps={
-        withBack
-          ? {
-              backText: lang.strings.back,
-            }
-          : undefined
-      }
+      headerProps={parseHeaderProps({ withHelp, withEmail, lang })}
+      footerProps={parseFooterProps({ withBack, lang })}
     >
       {children}
     </OnboardingLayout>
