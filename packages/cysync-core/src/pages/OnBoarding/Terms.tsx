0032092107b1ba41ecaccb678a258cd108517cfe
TejasvCypherock <107414753+TejasvCypherock@users.noreply.github.com>
2023-07-06 12:21:54 +0530

    feat: add checkpoint handling for onboarding (#76)
    * feat: add checkpoint handling for onboarding
    
    * refactor: review changes
    
    ---------
    
    Co-authored-by: Tejasv Sharma <tejasvonly@gmail.com>
    Co-authored-by: irshadCypherock <80461749+irshadCypherock@users.noreply.github.com>

--- packages/cysync-core/src/pages/OnBoarding/Terms.tsx
+++ packages/cysync-core/src/pages/OnBoarding/Terms.tsx
@@ -23,7 +23,7 @@ import React, {
 
 import { routes } from '~/constants';
 import { useLockscreen } from '~/context';
-import { useNavigateTo } from '~/hooks';
+import { useNavigateTo, useOnboardingCheckpoint } from '~/hooks';
 import { selectLanguage, useAppSelector } from '~/store';
 import { keyValueStore } from '~/utils';
 
@@ -123,6 +123,8 @@ export const Terms: FC = () => {
   const lang = useAppSelector(selectLanguage);
   const [isChecked, setIsChecked] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  useOnboardingCheckpoint();
 
   const fetchTerms = async () => {
     setIsLoading(true);
