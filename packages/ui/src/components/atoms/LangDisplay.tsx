0e81e9474333f123fa5d3f8b66d24cde5381db77
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2023-07-10 13:18:21 +0530

    test: fixed the e2e tests usage screen

--- packages/ui/src/components/atoms/LangDisplay.tsx
+++ packages/ui/src/components/atoms/LangDisplay.tsx
@@ -8,23 +8,11 @@ interface LangDisplayProps {
 const parseVariables = (templateStr: string, variables = {}) =>
   templateStr.replace(/\${(.*?)}/g, (x, g) => (variables as any)[g] ?? '');
 
-const parseLineBreaks = (text: string) => (
-  <>
-    {' '}
-    {text.split('\n').map((item, index, arr) => (
-      <React.Fragment key={`line breaks ${item}`}>
-        {item}
-        {arr.length - 1 !== index && <br />}
-      </React.Fragment>
-    ))}
-  </>
+const BaseLangDisplay: React.FC<LangDisplayProps> = ({ text, variables }) => (
+  <span style={{ whiteSpace: 'pre-wrap' }}>
+    {parseVariables(text, variables)}
+  </span>
 );
-const BaseLangDisplay: React.FC<LangDisplayProps> = ({ text, variables }) => {
-  const parsedText = parseVariables(text, variables);
-  const parsedTextWithLineBreaks = parseLineBreaks(parsedText);
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{parsedTextWithLineBreaks}</>;
-};
 
 BaseLangDisplay.defaultProps = {
   variables: undefined,
