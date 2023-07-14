ae898d430ed49be7947bf8709281d4631f280d95
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2023-07-14 16:53:53 +0530

    chore: linted the files

--- packages/cysync-core/src/bgTask/walletSyncTask/index.tsx
+++ packages/cysync-core/src/bgTask/walletSyncTask/index.tsx
@@ -2,13 +2,7 @@ import React, { useEffect } from 'react';
 
 import { openWalletSyncErrorDialog, syncWalletsWithDevice } from '~/actions';
 import { useDevice } from '~/context';
-import {
-  selectWallets,
-  setDeletedWallets,
-  useAppDispatch,
-  useAppSelector,
-} from '~/store';
-import { IWallet } from '@cypherock/db-interfaces';
+import { selectWallets, useAppDispatch, useAppSelector } from '~/store';
 
 export const WalletSyncTask: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -29,19 +23,7 @@ export const WalletSyncTask: React.FC = () => {
   }, [connection]);
 
   useEffect(() => {
-    console.log('useEffect', deletedWallets);
-    const newIW = {
-      name: 'fregerg',
-      hasPin: false,
-      hasPassphrase: false,
-      // foreign keys
-      deviceId: 'fefwef',
-      __id: 'asfw',
-    } as IWallet;
-    if (deletedWallets.length === 0) {
-      console.log('changed', [...deletedWallets, newIW]);
-      setDeletedWallets([...deletedWallets, newIW]);
-    } else onDeleteChange();
+    if (deletedWallets.length > 0) onDeleteChange();
   }, [deletedWallets]);
 
   return null;
