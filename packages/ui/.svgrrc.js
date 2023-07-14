4fc3c55325d66bc04590809c74cc5738c0c72776
Amneesh Singh <natto@weirdnatto.in>
2023-07-14 16:26:48 +0530

    refactor: add components-esque framework for SVGs
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

---
+++ packages/ui/.svgrrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  typescript: true,
+  template: (variables, { tpl, options }) => {
+    // use SvgProps and SvgStyle from the import
+    const componentNameWithType = `${variables.componentName}: React.FC<SvgProps>`;
+    variables.jsx.openingElement.name.name =
+      variables.jsx.closingElement.name.name = 'Svg';
+
+    return tpl`
+import React from 'react'
+import { SvgStyle as Svg, SvgProps } from '../Svg';
+
+const ${componentNameWithType} = ({stroke, fill, ...props}) => (
+    ${variables.jsx}
+);
+
+${variables.exports};
+`;
+  },
+  jsx: {
+    babelConfig: {
+      plugins: [
+        [
+          '@svgr/babel-plugin-add-jsx-attribute',
+          {
+            elements: ['path', 'rect', 'circle', 'line'],
+            attributes: [
+              {
+                name: 'fill',
+                value: 'fill',
+                literal: true,
+              },
+              {
+                name: 'stroke',
+                value: 'stroke',
+                literal: true,
+              },
+            ],
+          },
+        ],
+      ],
+    },
+  },
+};
--- packages/ui/README.md
+++ packages/ui/README.md
@@ -8,3 +8,12 @@
   defined in `i18n`.
 - All the texts should be displayed via `<LangDisplay>` component.
 - Define and export types for props.
+
+## Icons
+
+- React components for the icons can be generated from the SVGs in `./icons`
+  using `pnpm build:icons` -`fill` and `stroke` can be passed to such components and those will replace
+  the fill and stroke props of the child elements such as `path` ,`circle`,
+  `line`, and `rect`.
+- All the other SVG attributes such as `width` and `height` will be passed to
+  the root `<svg>` tag instead
