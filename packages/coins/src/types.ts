79c8f2ee2e034d64a76d6526a6437e4b6a136a52
Md Irshad Ansari <irshadjsr21@gmail.com>
2023-07-15 18:26:16 +0530

    feat: added coin-support aggregation layer

--- packages/coins/src/types.ts
+++ packages/coins/src/types.ts
@@ -4,6 +4,15 @@ export interface ICoinUnit {
   magnitude: number;
 }
 
+export const coinFamiliesMap = {
+  bitcoin: 'bitcoin',
+  evm: 'evm',
+  near: 'near',
+  solana: 'solana',
+} as const;
+
+export type CoinFamily = (typeof coinFamiliesMap)[keyof typeof coinFamiliesMap];
+
 export interface ICoinInfo {
   id: string;
   name: string;
--- packages/ui/src/components/molecules/ProgressLine.tsx
+++ packages/ui/src/components/molecules/ProgressLine.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+
 import { Container, Flex } from '../atoms';
 
 const bgColor = (activeTab: number, index: number) => {
