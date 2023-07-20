8edb8ff02461b45ad18d004ed21ddcc1e241278a
Subin S K <subinsk284@gmail.com>
2023-07-20 13:00:48 +0530

    fix: review changes

--- packages/cysync-core/src/pages/MainApp/Portfolio.tsx
+++ packages/cysync-core/src/pages/MainApp/Portfolio.tsx
@@ -1,16 +1,27 @@
 import { Container, Flex } from '@cypherock/cysync-ui';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { openWalletActionsDialog } from '~/actions';
 import { Topbar } from '~/components';
 import { AssetAllocation } from '~/pages/MainApp/Components/AssetAllocation';
+
 import { SideBar } from './Components';
 
-export const Portfolio: FC = () => (
-  <Container height="screen" display="flex">
-    <SideBar />
-    <Flex direction="column" grow={1} $alignSelf="start">
-      <Topbar />
-      <AssetAllocation />
-    </Flex>
-  </Container>
-);
+export const Portfolio: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(openWalletActionsDialog());
+  }, []);
+
+  return (
+    <Container height="screen" display="flex">
+      <SideBar />
+      <Flex direction="column" grow={1} $alignSelf="start">
+        <Topbar />
+        <AssetAllocation />
+      </Flex>
+    </Container>
+  );
+};
