bd1172e67ec5618372bce2ce74987cad9a772af6
Tejasv Sharma <tejasvonly@gmail.com>
2023-07-27 15:34:27 +0530

    chore: remove obsolete changes

--- packages/cysync-core/src/pages/MainApp/Portfolio.tsx
+++ packages/cysync-core/src/pages/MainApp/Portfolio.tsx
@@ -1,33 +1,17 @@
-import { BtcSupport } from '@cypherock/coin-support-btc';
 import React, { FC, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { openWalletActionsDialog } from '~/actions';
-import { useDevice } from '~/context';
-import { DeviceTask, useDeviceTask } from '~/hooks';
-import { AssetAllocation, MainAppLayout } from '~/pages/MainApp/Components';import { getDB } from '~/utils';
-
+import { AssetAllocation, MainAppLayout } from '~/pages/MainApp/Components';
 import { selectLanguage, useAppSelector } from '~/store';
 
 export const Portfolio: FC = () => {
   const { strings } = useAppSelector(selectLanguage);
   const dispatch = useDispatch();
 
-  const test: DeviceTask<void> = async connection => {
-    if (!connection) return;
+  useEffect(() => {
     dispatch(openWalletActionsDialog());
-    const btcSupport = new BtcSupport();
-    const observable = btcSupport.receive({
-      db: getDB(),
-      connection,
-      walletId: '',
-      accountId: '',
-      coinId: '',
-    });
-    console.log({ observable });
-  };
-
-  useDeviceTask(test);
+  }, []);
 
   return (
     <MainAppLayout title={strings.portfolio.title}>
