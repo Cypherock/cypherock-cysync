9c0d3ceb4f74c42dee13de0868cf29c3ae3509ec
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-09-15 19:31:04 +0530

    feat: added scrollable container

--- packages/cysync-core/src/dialogs/AuthenticateX1Card/Dialogs/X1CardAuthProcess.tsx
+++ packages/cysync-core/src/dialogs/AuthenticateX1Card/Dialogs/X1CardAuthProcess.tsx
@@ -14,6 +14,7 @@ import {
   LeanBoxContainer,
   LeanBox,
   AlertBox,
+  ScrollableContainer,
 } from '@cypherock/cysync-ui';
 import React from 'react';
 
@@ -45,55 +46,62 @@ export const X1CardAuthProcess: React.FC = () => {
   ];
 
   return (
-    <DialogBox width={500} align="stretch" gap={0}>
+    <DialogBox width={500} align="stretch" gap={0} $maxHeight="90vh">
       <Flex direction="row" justify="flex-end" py={2} px={3}>
         <CloseButton onClick={onClose} />
       </Flex>
       <Divider variant="horizontal" />
-      <DialogBoxBody gap={0} p={0} m={0} align="stretch">
-        <Flex
-          pt={4}
-          pb={{ def: 2, lg: 4 }}
-          px={{ def: 3, lg: 5 }}
-          gap={{ def: 16, lg: 32 }}
-          direction="column"
-          align="center"
-        >
-          <DeviceScreenTapCard width={264} />
-          <Flex direction="column" gap={4} align="center">
-            <Typography color="white" $fontSize={20} $textAlign="center">
-              <LangDisplay text={title} />
-            </Typography>
-            <Typography color="muted" $fontSize={16} $textAlign="center">
-              <LangDisplay text={authX1Card.description} />
-            </Typography>
+      <ScrollableContainer>
+        <DialogBoxBody gap={0} p={0} m={0} align="stretch">
+          <Flex
+            pt={4}
+            pb={{ def: 2, lg: 4 }}
+            px={{ def: 3, lg: 5 }}
+            gap={{ def: 16, lg: 32 }}
+            direction="column"
+            align="center"
+          >
+            <DeviceScreenTapCard width={264} />
+            <Flex direction="column" gap={4} align="center">
+              <Typography color="white" $fontSize={20} $textAlign="center">
+                <LangDisplay text={title} />
+              </Typography>
+              <Typography color="muted" $fontSize={16} $textAlign="center">
+                <LangDisplay text={authX1Card.description} />
+              </Typography>
+            </Flex>
           </Flex>
-        </Flex>
-        <Flex
-          pt={2}
-          pb={{ def: 2, lg: 4 }}
-          px={{ def: 3, lg: 5 }}
-          gap={8}
-          direction="column"
-          align="stretch"
-        >
-          <LeanBoxContainer>
-            {stepsList.map(data => (
-              <LeanBox key={data.id} {...data} color="white" disabled={false} />
-            ))}
-          </LeanBoxContainer>
-        </Flex>
-        <Flex
-          pt={2}
-          pb={{ def: 2, lg: 4 }}
-          px={{ def: 3, lg: 5 }}
-          gap={8}
-          direction="column"
-          align="stretch"
-        >
-          <AlertBox subAlert={authX1Card.info} variant="warning" />
-        </Flex>
-      </DialogBoxBody>
+          <Flex
+            pt={2}
+            pb={{ def: 2, lg: 4 }}
+            px={{ def: 3, lg: 5 }}
+            gap={8}
+            direction="column"
+            align="stretch"
+          >
+            <LeanBoxContainer>
+              {stepsList.map(data => (
+                <LeanBox
+                  key={data.id}
+                  {...data}
+                  color="white"
+                  disabled={false}
+                />
+              ))}
+            </LeanBoxContainer>
+          </Flex>
+          <Flex
+            pt={2}
+            pb={{ def: 2, lg: 4 }}
+            px={{ def: 3, lg: 5 }}
+            gap={8}
+            direction="column"
+            align="stretch"
+          >
+            <AlertBox subAlert={authX1Card.info} variant="warning" />
+          </Flex>
+        </DialogBoxBody>
+      </ScrollableContainer>
     </DialogBox>
   );
 };
