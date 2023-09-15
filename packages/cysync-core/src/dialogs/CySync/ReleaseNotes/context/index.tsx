5ee5cfe0737f4182ab0aeb7f042643d89dea3798
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-09-15 16:57:35 +0530

    fix: removed tabs

--- packages/cysync-core/src/dialogs/CySync/ReleaseNotes/context/index.tsx
+++ packages/cysync-core/src/dialogs/CySync/ReleaseNotes/context/index.tsx
@@ -7,24 +7,9 @@ import React, {
   useMemo,
 } from 'react';
 
-import { ITabs, useTabsAndDialogs } from '~/hooks';
-import {
-  closeDialog,
-  selectLanguage,
-  useAppDispatch,
-  useAppSelector,
-} from '~/store';
-
-import { ReleaseNotes } from '../Dialogs';
+import { closeDialog, useAppDispatch } from '~/store';
 
 export interface ReleaseNotesDialogContextInterface {
-  tabs: ITabs;
-  isDeviceRequired: boolean;
-  currentTab: number;
-  currentDialog: number;
-  onNext: () => void;
-  goTo: (tab: number, dialog?: number) => void;
-  onPrevious: () => void;
   onClose: () => void;
 }
 
@@ -40,54 +25,17 @@ export interface ReleaseNotesDialogProviderProps {
 export const ReleaseNotesDialogProvider: FC<
   ReleaseNotesDialogProviderProps
 > = ({ children }) => {
-  const lang = useAppSelector(selectLanguage);
   const dispatch = useAppDispatch();
-  const deviceRequiredDialogsMap: Record<number, number[] | undefined> = {};
 
   const onClose = () => {
     dispatch(closeDialog('releaseNotes'));
   };
 
-  const tabs: ITabs = [
-    {
-      name: lang.strings.settings.tabs.app.title,
-      dialogs: [<ReleaseNotes key="release-notes" />],
-    },
-  ];
-
-  const {
-    onNext,
-    onPrevious,
-    goTo,
-    currentTab,
-    currentDialog,
-    isDeviceRequired,
-  } = useTabsAndDialogs({
-    deviceRequiredDialogsMap,
-    tabs,
-  });
-
   const ctx = useMemo(
     () => ({
-      isDeviceRequired,
-      currentTab,
-      currentDialog,
-      tabs,
-      onNext,
-      goTo,
-      onPrevious,
       onClose,
     }),
-    [
-      isDeviceRequired,
-      currentTab,
-      currentDialog,
-      tabs,
-      onNext,
-      goTo,
-      onPrevious,
-      onClose,
-    ],
+    [onClose],
   );
 
   return (
