549c2a0b93133c8a4534c8000bdeeb98d03957c8
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-10-23 10:52:17 +0530

    fix: prevent container from overflow

--- packages/cysync-core/src/dialogs/Send/index.tsx
+++ packages/cysync-core/src/dialogs/Send/index.tsx
@@ -49,7 +49,7 @@ export const SendFlow: FC = () => {
 
   return (
     <BlurOverlay>
-      <DialogBox direction="row" gap={0} width="full">
+      <DialogBox direction="row" gap={0} width="full" $maxHeight="90vh">
         <CloseConfirmationDialog
           isDialogVisible={showOnClose}
           setIsDialogVisible={setShowOnClose}
