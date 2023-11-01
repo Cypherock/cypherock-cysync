954ba52dc7f9c1c771a770e2a0b38d512c0f170a
Md Irshad Ansari <irshadjsr21@gmail.com>
2023-11-01 13:45:38 +0530

    fix: wallet connect deeplink for macos

--- apps/desktop/src/main/app.ts
+++ apps/desktop/src/main/app.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { release } from 'node:os';
 
 import { sleep } from '@cypherock/cysync-utils';
@@ -46,6 +47,16 @@ const shouldStartApp = () => {
 };
 
 const prepareApp = () => {
+  if (process.defaultApp) {
+    if (process.argv.length >= 2) {
+      app.setAsDefaultProtocolClient('cypherock', process.execPath, [
+        path.resolve(process.argv[1]),
+      ]);
+    }
+  } else {
+    app.setAsDefaultProtocolClient('cypherock');
+  }
+
   setupProcessEventHandlers();
   setupDependencies();
   setupIPCHandlers(ipcMain, getWebContents);
@@ -182,6 +193,15 @@ export default function createApp() {
       if (mainWindow.isMinimized()) mainWindow.restore();
       mainWindow.focus();
     }
+  });
+
+  app.on('open-url', (event, url) => {
+    // Handle deeplink for macos
+    logger.info('Deeplink received');
+    logger.info({ event, url });
+    event.preventDefault();
+
+    handleUriOpen(url);
   });
 
   app.on('activate', () => {
