6deb54405ed048ee822b8af8ce915d032b5986a5
Tejasv Sharma <tejasvonly@gmail.com>
2023-11-10 18:18:38 +0530

    fix: various bug fixes
    fixes:
    - https://app.clickup.com/t/9002019994/PRF-6393
    - https://app.clickup.com/t/9002019994/PRF-6532
    - https://app.clickup.com/t/9002019994/PRF-6401
    - Text change for transaction type
    - Update dialog priority to hide wallet sync error while in guided flow

--- packages/cysync-core/src/utils/transaction.ts
+++ packages/cysync-core/src/utils/transaction.ts
@@ -7,42 +7,14 @@ import {
   ITransaction,
   TransactionType,
   TransactionTypeMap,
-  TransactionStatus,
-  TransactionStatusMap,
 } from '@cypherock/db-interfaces';
 
 import { ILangState } from '..';
 
-export const getTransactionTypeToStringMap = (
-  strings: ILangState['strings'],
-): Record<TransactionType, string> => ({
-  [TransactionTypeMap.receive]: strings.history.transactionType.receive,
-  [TransactionTypeMap.send]: strings.history.transactionType.send,
-});
-
-export const getTransactionStatusToStringMap = (
-  strings: ILangState['strings'],
-): Record<TransactionStatus, string> => ({
-  [TransactionStatusMap.success]: '',
-  [TransactionStatusMap.pending]: strings.history.transactionType.pending,
-  [TransactionStatusMap.failed]: strings.history.transactionType.failed,
-});
-
 export const getDisplayTransactionType = (
   t: ITransaction,
   strings: ILangState['strings'],
-) => {
-  const transactionTypeToStringMap = getTransactionTypeToStringMap(strings);
-  const transactionStatusToStringMap = getTransactionStatusToStringMap(strings);
-
-  let text = transactionTypeToStringMap[t.type];
-
-  if (transactionStatusToStringMap[t.status]) {
-    text += ` ${transactionStatusToStringMap[t.status]}`;
-  }
-
-  return text;
-};
+) => (strings.history.transactionStatus as any)[t.type][t.status];
 
 export const transactionIconMap: Record<TransactionType, React.FC<SvgProps>> = {
   [TransactionTypeMap.receive]: ArrowReceivedIcon,
