1214d6ae43d4a9a89d28dd058a15f1ef2866b6c3
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-11-11 00:10:17 +0530

    feat: added device connection animation

--- packages/ui/src/assets/videos/index.ts
+++ packages/ui/src/assets/videos/index.ts
@@ -1,13 +1,13 @@
-import deviceConnectionAnimationVideo from './device-connection-animation-video.webm';
+import deviceConnectionAnimation2DVideo from './device-connection-animation-2d.webm';
+import distributeToLocationsAnimationVideo from './distribute-to-locations-animation.webm';
+import enterSeedphraseAnimationVideo from './enter-seedphrase-animation.webm';
 import tapAllCardDeviceAnimation2DVideo from './tap-all-card-device-animation-2d.webm';
 import tapAnyCardDeviceAnimation2DVideo from './tap-any-card-device-animation-2d.webm';
-import enterSeedphraseAnimationVideo from './enter-seedphrase-animation.webm';
-import distributeToLocationsAnimationVideo from './distribute-to-locations-animation.webm';
 
 export {
-  deviceConnectionAnimationVideo,
+  deviceConnectionAnimation2DVideo,
+  distributeToLocationsAnimationVideo,
+  enterSeedphraseAnimationVideo,
   tapAllCardDeviceAnimation2DVideo,
   tapAnyCardDeviceAnimation2DVideo,
-  enterSeedphraseAnimationVideo,
-  distributeToLocationsAnimationVideo,
 };
