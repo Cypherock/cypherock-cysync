667a18980654db866f21f91fa201804826a864bf
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-12-12 02:38:26 +0530

    fix: prevent overwrite of dialog props

--- packages/cysync-core/src/store/dialog/index.ts
+++ packages/cysync-core/src/store/dialog/index.ts
@@ -86,6 +86,7 @@ export const dialogSlice = createSlice({
       state,
       payload: PayloadAction<{ name: DialogName; data: any }>,
     ) => {
+      if (state[payload.payload.name].isOpen) return;
       state[payload.payload.name].isOpen = true;
       (state[payload.payload.name] as any).data = payload.payload.data;
     },
