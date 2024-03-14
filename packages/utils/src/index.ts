68b3f522e8f2716ec0eb8f76f9a4cfcb494bb693
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2024-03-14 19:59:43 +0530

    fix!: removed `createComparator`

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,5 @@
 export * from './assert';
 export * from './bigNumber';
-export * from './comparator';
 export * from './getEnv';
 export * from './http';
 export * from './isSubset';
--- packages/utils/tests/comparator/__fixtures__/index.ts
+++
@@ -1,9 +0,0 @@
-import { invalid } from './invalid';
-import { valid } from './valid';
-
-export * from './types';
-
-export const fixtures = {
-  valid,
-  invalid,
-};
