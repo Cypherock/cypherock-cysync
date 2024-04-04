c25efafcee92cc1c9f1eac51f0641edddff73b10
Irshad Ansari <40335357+irshadjsr21@users.noreply.github.com>
2024-04-04 16:51:23 +0530

    feat: added download csv history feature (#428)

--- packages/core-services/src/index.ts
+++ packages/core-services/src/index.ts
@@ -3,4 +3,5 @@ export * from './db';
 export * from './account';
 export * from './portfolio';
 export * from './address';
+export * from './transactions';
 export { updateLogger } from './utils/logger';
