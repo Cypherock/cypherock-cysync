a04e06be757008c83d98c832d39fb8c909212844
Ishaan28malik <ishaanmalikrm7@gmail.com>
2024-04-18 20:01:30 +0530

    feat: fixes

---
+++ .changeset/flat-ways-fly.md
@@ -0,0 +1,6 @@
+---
+'@cypherock/cysync-ui': patch
+'@cypherock/cysync-desktop': patch
+---
+
+ui checkbox privacy policy component for inheritance
---
+++ packages/ui/src/stories/atoms/CheckoutPrivacyPolicy.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CheckoutPrivacyPolicy } from '../../components';
+
+const meta: Meta<typeof CheckoutPrivacyPolicy> = {
+  component: CheckoutPrivacyPolicy,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Info: Story = {
+  args: {},
+};
