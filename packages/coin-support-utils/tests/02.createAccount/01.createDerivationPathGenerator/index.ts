216280dcd2d0d91bcc535df3db5d0f1b4336faf4
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2024-05-02 10:11:27 +0530

    refactor: corrected order of statements
    Co-authored-by: Irshad Ansari <40335357+irshadjsr21@users.noreply.github.com>

--- packages/coin-support-utils/tests/02.createAccount/01.createDerivationPathGenerator/index.ts
+++ packages/coin-support-utils/tests/02.createAccount/01.createDerivationPathGenerator/index.ts
@@ -6,8 +6,8 @@ describe('01. Create Derivation Path Generator', () => {
   fixtures.valid.forEach(({ name, input, output }) => {
     test(name, () => {
       const generator = createDerivationPathGenerator(input.basePath);
-      const result = generator(input.existingDerivationPaths, input.limit);
       expect(generator).toBeInstanceOf(Function);
+      const result = generator(input.existingDerivationPaths, input.limit);
       expect(result).toStrictEqual(output);
     });
   });
