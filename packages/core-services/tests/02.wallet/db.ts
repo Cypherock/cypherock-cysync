f135694108c86a39f28ee1861db2d97e3a8d337a
moayaan1911 <ayaangames@gmail.com>
2024-05-09 06:38:06 +0530

    test(wallet): add tests for wallet db.ts

--- packages/core-services/tests/02.wallet/db.ts
+++ packages/core-services/tests/02.wallet/db.ts
@@ -1,6 +1,7 @@
-import { IDatabase, IWallet } from '@cypherock/db-interfaces';
+import { IDatabase } from '@cypherock/db-interfaces';
 import { mockDb } from '../__mocks__/mocks';
 import { insertWallets, updateWallets, deleteWallets } from '../../src';
+import { walletTestData } from '../__fixtures__/db';
 
 describe('Wallet Operations', () => {
   beforeEach(() => {
@@ -8,147 +9,119 @@ describe('Wallet Operations', () => {
   });
 
   describe('insertWallets', () => {
-    test('should insert wallets into the database', async () => {
-      const wallets: IWallet[] = [
-        {
-          deviceId: '1',
-          name: 'Wallet 1',
-          hasPassphrase: false,
-          hasPin: false,
-        },
-        {
-          deviceId: '2',
-          name: 'Wallet 2',
-          hasPassphrase: false,
-          hasPin: false,
-        },
-      ];
-
-      await insertWallets(mockDb as IDatabase, wallets);
-      expect((mockDb as IDatabase).wallet.insert).toHaveBeenCalledWith(wallets);
-    });
-
-    test('should not insert wallets if the array is empty', async () => {
-      const wallets: IWallet[] = [];
-
-      await insertWallets(mockDb as IDatabase, wallets);
-      expect((mockDb as IDatabase).wallet.insert).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('updateWallets', () => {
-    test('should update wallets in the database', async () => {
-      const wallets: IWallet[] = [
-        {
-          __id: '1',
-          deviceId: '1',
-          name: 'Updated Wallet 1',
-          hasPassphrase: true,
-          hasPin: false,
-        },
-        {
-          __id: '2',
-          deviceId: '2',
-          name: 'Updated Wallet 2',
-          hasPassphrase: false,
-          hasPin: true,
-        },
-      ];
-
-      await updateWallets(mockDb as IDatabase, wallets);
-
-      expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledTimes(
-        wallets.length,
-      );
-      wallets.forEach(wallet => {
-        expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledWith(
-          { __id: wallet.__id },
-          wallet,
+    walletTestData.insertWallets.valid.forEach((testCase, index) => {
+      test(`should insert wallets into the database - test case ${
+        index + 1
+      }`, async () => {
+        await insertWallets(mockDb as IDatabase, testCase);
+        expect((mockDb as IDatabase).wallet.insert).toHaveBeenCalledWith(
+          testCase,
         );
       });
     });
 
-    test('should skip wallets without an __id', async () => {
-      const wallets: IWallet[] = [
-        {
-          __id: '1',
-          deviceId: '1',
-          name: 'Updated Wallet 1',
-          hasPassphrase: true,
-          hasPin: false,
-        },
-        { deviceId: '2', name: 'Wallet 2', hasPassphrase: false, hasPin: true },
-      ];
+    walletTestData.insertWallets.invalid.forEach((testCase, index) => {
+      test(`should not insert wallets if the array is empty - test case ${
+        index + 1
+      }`, async () => {
+        await insertWallets(mockDb as IDatabase, testCase);
+        expect((mockDb as IDatabase).wallet.insert).not.toHaveBeenCalled();
+      });
+    });
+  });
 
-      await updateWallets(mockDb as IDatabase, wallets);
+  describe('updateWallets', () => {
+    walletTestData.updateWallets.valid.forEach((testCase, index) => {
+      test(`should update wallets in the database - test case ${
+        index + 1
+      }`, async () => {
+        await updateWallets(mockDb as IDatabase, testCase);
 
-      expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledTimes(1);
-      expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledWith(
-        { __id: '1' },
-        wallets[0],
-      );
+        expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledTimes(
+          testCase.length,
+        );
+        testCase.forEach(wallet => {
+          expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledWith(
+            { __id: wallet.__id },
+            wallet,
+          );
+        });
+      });
+    });
+
+    walletTestData.updateWallets.invalid.forEach((testCase, index) => {
+      test(`should skip wallets without an __id - test case ${
+        index + 1
+      }`, async () => {
+        await updateWallets(mockDb as IDatabase, testCase);
+
+        const validWallets = testCase.filter(wallet => wallet.__id);
+        expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledTimes(
+          validWallets.length,
+        );
+        validWallets.forEach(wallet => {
+          expect((mockDb as IDatabase).wallet.update).toHaveBeenCalledWith(
+            { __id: wallet.__id },
+            wallet,
+          );
+        });
+      });
     });
   });
 
   describe('deleteWallets', () => {
-    test('should delete wallets from the database', async () => {
-      const wallets: IWallet[] = [
-        {
-          __id: '1',
-          deviceId: '1',
-          name: 'Wallet 1',
-          hasPassphrase: false,
-          hasPin: false,
-        },
-        {
-          __id: '2',
-          deviceId: '2',
-          name: 'Wallet 2',
-          hasPassphrase: false,
-          hasPin: false,
-        },
-      ];
+    walletTestData.deleteWallets.valid.forEach((testCase, index) => {
+      test(`should delete wallets from the database - test case ${
+        index + 1
+      }`, async () => {
+        await deleteWallets(mockDb as IDatabase, testCase);
 
-      await deleteWallets(mockDb as IDatabase, wallets);
-
-      wallets.forEach(wallet => {
-        expect((mockDb as IDatabase).account.remove).toHaveBeenCalledWith({
-          walletId: wallet.__id,
-        });
-        expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledWith({
-          __id: wallet.__id,
-        });
-        expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledWith({
-          walletId: wallet.__id,
+        testCase.forEach(wallet => {
+          expect((mockDb as IDatabase).account.remove).toHaveBeenCalledWith({
+            walletId: wallet.__id,
+          });
+          expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledWith({
+            __id: wallet.__id,
+          });
+          expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledWith(
+            {
+              walletId: wallet.__id,
+            },
+          );
         });
       });
     });
 
-    test('should skip wallets without an __id', async () => {
-      const wallets: IWallet[] = [
-        {
-          __id: '1',
-          deviceId: '1',
-          name: 'Wallet 1',
-          hasPassphrase: false,
-          hasPin: false,
-        },
-        { deviceId: '2', name: 'Wallet 2', hasPassphrase: false, hasPin: true },
-      ];
+    walletTestData.deleteWallets.invalid.forEach((testCase, index) => {
+      test(`should skip wallets without an __id - test case ${
+        index + 1
+      }`, async () => {
+        await deleteWallets(mockDb as IDatabase, testCase);
 
-      await deleteWallets(mockDb as IDatabase, wallets);
+        const validWallets = testCase.filter(wallet => wallet.__id);
+        expect((mockDb as IDatabase).account.remove).toHaveBeenCalledTimes(
+          validWallets.length,
+        );
+        expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledTimes(
+          validWallets.length,
+        );
+        expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledTimes(
+          validWallets.length,
+        );
 
-      expect((mockDb as IDatabase).account.remove).toHaveBeenCalledTimes(1);
-      expect((mockDb as IDatabase).account.remove).toHaveBeenCalledWith({
-        walletId: '1',
-      });
-      expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledTimes(1);
-      expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledWith({
-        __id: '1',
-      });
-      expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledTimes(1);
-      expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledWith({
-        walletId: '1',
+        validWallets.forEach(wallet => {
+          expect((mockDb as IDatabase).account.remove).toHaveBeenCalledWith({
+            walletId: wallet.__id,
+          });
+          expect((mockDb as IDatabase).wallet.remove).toHaveBeenCalledWith({
+            __id: wallet.__id,
+          });
+          expect((mockDb as IDatabase).transaction.remove).toHaveBeenCalledWith(
+            {
+              walletId: wallet.__id,
+            },
+          );
+        });
       });
     });
   });
