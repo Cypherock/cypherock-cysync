1f5e8f42a7e92556e6eacbdfb764f7ce747a8f1f
Md Irshad Ansari <irshadjsr21@gmail.com>
2024-06-25 14:05:02 +0530

    refactor: code

--- packages/coin-support-tron/src/config.ts
+++ packages/coin-support-tron/src/config.ts
@@ -2,5 +2,4 @@ import { getEnvVariable } from '@cypherock/cysync-utils';
 
 export const config = {
   API_CYPHEROCK: getEnvVariable('API_CYPHEROCK', 'https://api.cypherock.com'),
-  NOW_NODES_API_KEY: getEnvVariable('NOW_NODES_API_KEY', 'TEST'),
 };
