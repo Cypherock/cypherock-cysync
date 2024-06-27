c92ebc4acfbb64cc866a11f18b59abc1c5998827
Championrunner <27343592+Ishaan28malik@users.noreply.github.com>
2024-06-27 16:39:45 +0530

    Delete packages/cysync-core-constants/dist/cjs/constants.d.ts

--- packages/cysync-core-constants/dist/cjs/constants.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const constants: {
-    linuxPermissionScript: string;
-    appDownloadLink: string;
-    walletConnectProjectId: string;
-    privacyPolicyLink: string;
-    termsOfUseLink: string;
-    tutorialLink: string;
-};
-//# sourceMappingURL=constants.d.ts.map
