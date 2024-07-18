9b1ef54f05afceb76398e1d29eff6aa3cede64df
Ishaan28malik <ishaanmalikrm7@gmail.com>
2024-07-18 17:49:45 +0530

    feat: pr review changes on svg nd state management

--- packages/ui/src/components/molecules/ManyInMany.tsx
+++ packages/ui/src/components/molecules/ManyInMany.tsx
@@ -2,57 +2,47 @@ import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 import { Flex } from '../atoms';
 import {
-  MimDefaultWallet,
   InfoItalicsIcon,
   manyInManyBackground,
   manyIManyDefault,
+  MimHoverWallet,
+  MimDefaultWallet,
 } from '../../assets';
 import { WidthProps, width } from '../utils';
-import { svgGradients } from '../GlobalStyles';
 
-const StyledDisableContainer = styled.div<WidthProps>`
-  position: relative;
-  background: ${({ theme }) => theme.palette.background.disabledBackground};
-  overflow: hidden;
-  border-radius: 8px;
-  width: 276px;
-  height: 128px;
-  padding: 24px 16px;
-  color: ${({ theme }) => theme.palette.border.separator};
-  cursor: not-allowed;
-  ${width}
-`;
+const getBoxShadow = ($isSelected: boolean, theme: any, disabled: boolean) => {
+  if (disabled) return 'none';
+  if ($isSelected)
+    return `0px 0px 12px 4px ${theme.palette.shadow.selected} inset`;
+  return `0px 0px 12px 4px ${theme.palette.shadow.selected}`;
+};
 
-const WalletDisableImage = styled.div`
-  margin-bottom: 16px;
-`;
+const getBackground = ($isSelected: boolean, theme: any, disabled: boolean) => {
+  if (disabled) return theme.palette.background.disabledBackground;
+  if ($isSelected) return theme.palette.text.selected;
+  return theme.palette.selected.default;
+};
 
-const StyledRedInfoImage = styled.div`
-  position: absolute;
-  right: 10px;
-  top: 10px;
-`;
-
-const StyledContainer = styled.div<{ $isSelected: boolean } & WidthProps>`
+const StyledContainer = styled.div<
+  { $isSelected: boolean; disabled: boolean } & WidthProps
+>`
   position: relative;
   overflow: hidden;
   width: 276px;
   height: 128px;
   padding: 24px 16px;
-  color: ${({ theme }) => theme.palette.bullet.white};
-  cursor: pointer;
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.palette.border.separator : theme.palette.bullet.white};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   border-radius: 8px;
-  border: ${({ $isSelected, theme }) =>
-    $isSelected ? `1px solid ${theme.palette.border.selected}` : 'none'};
-  box-shadow: ${({ $isSelected, theme }) =>
-    !$isSelected
-      ? `0px 0px 12px 4px ${theme.palette.shadow.selected}`
-      : `0px 0px 12px 4px ${theme.palette.shadow.selected} inset`};
-  background: ${({ $isSelected, theme }) =>
-    !$isSelected
-      ? theme.palette.selected.default
-      : theme.palette.text.selected};
-
+  border: ${({ $isSelected, theme, disabled }) =>
+    $isSelected && !disabled
+      ? `1px solid ${theme.palette.border.selected}`
+      : 'none'};
+  box-shadow: ${({ $isSelected, theme, disabled }) =>
+    getBoxShadow($isSelected, theme, disabled)};
+  background: ${({ $isSelected, theme, disabled }) =>
+    getBackground($isSelected, theme, disabled)};
   ${width}
 
   &::after {
@@ -63,41 +53,60 @@ const StyledContainer = styled.div<{ $isSelected: boolean } & WidthProps>`
     top: 0;
     left: 0;
     z-index: 0;
-    background-image: ${({ $isSelected }) =>
-      !$isSelected ? `url(${manyIManyDefault})` : 'none'};
+    background-image: ${({ $isSelected, disabled }) =>
+      !disabled && !$isSelected ? `url(${manyIManyDefault})` : 'none'};
     background-position: bottom center;
     background-repeat: no-repeat;
     background-size: 280px;
     transition: transform 0.8s ease-in-out;
     transform-origin: bottom center;
   }
+
   &:hover::after {
-    transform: ${({ $isSelected }) =>
-      !$isSelected ? 'translateX(-30%) rotate(45deg) scale(1.5)' : 'none'};
+    transform: ${({ $isSelected, disabled }) =>
+      !disabled && !$isSelected
+        ? 'translateX(-30%) rotate(45deg) scale(1.5)'
+        : 'none'};
     background-position: bottom left 180%;
     width: 167%;
     height: 175%;
     background-size: 496px;
-    background-image: ${({ $isSelected }) =>
-      !$isSelected ? `url(${manyInManyBackground})` : 'none'};
+    background-image: ${({ $isSelected, disabled }) =>
+      !disabled && !$isSelected ? `url(${manyInManyBackground})` : 'none'};
   }
-  &:hover {
-    background: ${({ $isSelected, theme }) =>
-      !$isSelected
-        ? theme.palette.selected.hover
-        : theme.palette.text.selected};
-  }
-  position: relative;
-  z-index: 1;
 
-  &:hover .wallet-icon {
-    stroke: ${({ $isSelected }) =>
-      !$isSelected ? `url(#${svgGradients.gold})` : 'none'};
+  &:hover {
+    background: ${({ $isSelected, theme, disabled }) =>
+      getBackground($isSelected, theme, disabled)};
+  }
+
+  .default-wallet {
+    display: block;
+  }
+
+  .hover-wallet {
+    display: none;
+  }
+
+  &:hover .default-wallet {
+    display: ${({ disabled, $isSelected }) =>
+      !disabled && !$isSelected ? 'none' : 'block'};
+  }
+
+  &:hover .hover-wallet {
+    display: ${({ disabled, $isSelected }) =>
+      !disabled && !$isSelected ? 'block' : 'none'};
   }
 
   &:hover .date-label {
     font-size: ${({ $isSelected }) => (!$isSelected ? '15px' : '14px')};
   }
+`;
+
+const StyledRedInfoImage = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 10px;
 `;
 
 const StyledDateLabel = styled.div<{ $isSelected: boolean }>`
@@ -122,44 +131,50 @@ export const ManyInMany: FC<ManyInManyProps> = ({
   ...restProps
 }) => {
   const [isSelected, setIsSelected] = useState(false);
-  const [isHover, setIsHover] = useState(false);
 
-  return !disabled ? (
+  return (
     <StyledContainer
-      onClick={() => setIsSelected(!isSelected)}
+      onClick={() => !disabled && setIsSelected(!isSelected)}
       $isSelected={isSelected}
-      onMouseEnter={() => setIsHover(true)}
-      onMouseLeave={() => setIsHover(false)}
+      disabled={disabled}
       {...restProps}
     >
-      <Flex
-        align="center"
-        direction="column"
-        $height="100%"
-        justify="center"
-        $zIndex={2}
-      >
-        {isHover && !isSelected ? (
-          <MimDefaultWallet $zIndex={1} stroke={`url(#${svgGradients.gold})`} />
-        ) : (
-          <MimDefaultWallet $zIndex={1} />
-        )}
-        <StyledDateLabel className="date-label" $isSelected={isSelected}>
-          {title}
-        </StyledDateLabel>
-      </Flex>
+      {!disabled ? (
+        <Flex
+          align="center"
+          direction="column"
+          $height="100%"
+          justify="center"
+          $zIndex={2}
+        >
+          <div className="default-wallet">
+            <MimDefaultWallet $zIndex={1} />
+          </div>
+          <div className="hover-wallet">
+            <MimHoverWallet $zIndex={1} />
+          </div>
+          <StyledDateLabel className="date-label" $isSelected={isSelected}>
+            {title}
+          </StyledDateLabel>
+        </Flex>
+      ) : (
+        <>
+          <StyledRedInfoImage>
+            <InfoItalicsIcon fill="#FF624C" />
+          </StyledRedInfoImage>
+          <Flex
+            align="center"
+            direction="column"
+            $height="100%"
+            justify="center"
+          >
+            <div>
+              <MimDefaultWallet $zIndex={1} stroke="#39322C" />
+            </div>
+            {title}
+          </Flex>
+        </>
+      )}
     </StyledContainer>
-  ) : (
-    <StyledDisableContainer>
-      <StyledRedInfoImage>
-        <InfoItalicsIcon fill="#FF624C" />
-      </StyledRedInfoImage>
-      <Flex align="center" direction="column" $height="100%" justify="center">
-        <WalletDisableImage>
-          <MimDefaultWallet $zIndex={1} stroke="#39322C" />
-        </WalletDisableImage>
-        {title}
-      </Flex>
-    </StyledDisableContainer>
   );
 };
