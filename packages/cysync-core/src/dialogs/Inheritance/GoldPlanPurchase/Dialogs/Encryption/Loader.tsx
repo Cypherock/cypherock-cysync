f14e67bd8f78ae00a78df168fd21743dd2476a13
TejasvOnly <tejasvonly@gmail.com>
2024-10-05 07:35:32 -0700

    fix: added delay in loader dialogs for inheritance

--- packages/cysync-core/src/dialogs/Inheritance/GoldPlanPurchase/Dialogs/Encryption/Loader.tsx
+++ packages/cysync-core/src/dialogs/Inheritance/GoldPlanPurchase/Dialogs/Encryption/Loader.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { LoaderDialog } from '~/components';
 
+import { LoaderDialog } from '~/components';
+import { useCallbackAfterCountdown } from '~/hooks';
 import { selectLanguage, useAppSelector } from '~/store';
 
 import { useInheritanceGoldPlanPurchaseDialog } from '../../context';
@@ -12,14 +13,21 @@ export const EncryptionLoader: React.FC = () => {
 
   const strings = lang.strings.inheritanceGoldPlanPurchase.encryption.loading;
 
+  const onNextCallback = useCallbackAfterCountdown(onNext, 3000);
+
   useEffect(() => {
+    onNextCallback.setStartTime(Date.now());
     setupPlan();
   }, [retryIndex]);
 
   useEffect(() => {
     if (isSetupPlanCompleted) {
-      onNext();
+      onNextCallback.start();
     }
+
+    return () => {
+      onNextCallback.stop();
+    };
   }, [isSetupPlanCompleted]);
 
   return <LoaderDialog title={strings.title} subtext={strings.subTitle} />;
