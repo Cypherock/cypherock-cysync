59f7c7045939421708884748f27a1b84aabcf521
TejasvOnly <tejasvonly@gmail.com>
2024-11-02 02:45:26 -0700

    fix: removed redundant feilds

--- packages/cysync-core/src/utils/error/index.ts
+++ packages/cysync-core/src/utils/error/index.ts
@@ -30,17 +30,13 @@ export * from './types';
  */
 const identifyServerErrors = (error: any) => {
   if (error?.isAxiosError) {
-    if (error.response?.data && error.response?.data?.binanceError) {
-      return new BinanceServerError(
-        error.response.data.binanceErrorCode,
-        undefined,
-        {
-          advanceText: error?.response?.data?.cysyncError,
-          responseBody: error?.response?.data,
-          url: error?.request?.url,
-          status: error?.response?.status,
-        },
-      );
+    if (error.response?.data?.isBinanceError) {
+      return new BinanceServerError(error.response.data.code, undefined, {
+        advanceText: error.response.data.cysyncError,
+        responseBody: error.response.data,
+        url: error.request?.url,
+        status: error.response.status,
+      });
     }
     if (error.response) {
       return new ServerError(ServerErrorType.UNKNOWN_ERROR, undefined, {
