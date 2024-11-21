0dfa966947ab16da7c34fa1c8ea28e554b16ed38
AryanCypherock <aryan@cypherock.com>
2024-11-21 14:55:33 +0530

    feat: added close button in plan login (#632)

--- packages/cysync-core/src/dialogs/Inheritance/PlanLogin/Dialogs/WalletAuth.tsx
+++ packages/cysync-core/src/dialogs/Inheritance/PlanLogin/Dialogs/WalletAuth.tsx
@@ -2,8 +2,10 @@ import { InheritanceWalletAuthDeviceEvent } from '@cypherock/app-support-inherit
 import {
   ArrowRightIcon,
   Check,
+  CloseButton,
   DialogBox,
   DialogBoxBody,
+  DialogBoxHeader,
   Flex,
   LangDisplay,
   LeanBox,
@@ -95,6 +97,9 @@ export const WalletAuth: React.FC = () => {
 
   return (
     <DialogBox width={800} onClose={onClose} $maxHeight="90vh">
+      <DialogBoxHeader direction="row" justify="flex-end" py={2} px={3}>
+        <CloseButton width={24} onClick={onClose} />
+      </DialogBoxHeader>
       <ScrollableContainer>
         <DialogBoxBody px={5} py={4} gap={0}>
           <Flex
