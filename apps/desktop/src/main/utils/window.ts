efe2eeca2f341effa0200b8cb3ec0aa0798ea215
TejasvOnly <tejasvonly@gmail.com>
2024-11-23 02:10:14 -0800

    fix: disable dev tools on prod

--- apps/desktop/src/main/utils/window.ts
+++ apps/desktop/src/main/utils/window.ts
@@ -99,6 +99,7 @@ export const createWindowAndOpenUrl = (
       preload: windowUrls.preload,
       nodeIntegration: false,
       contextIsolation: true,
+      devTools: !config.IS_PRODUCTION && !config.IS_TEST,
     },
   });
 
