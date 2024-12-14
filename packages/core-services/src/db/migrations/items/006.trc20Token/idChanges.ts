9f2b11ea11b720be8cd3953bef63eaa333267cf6
TejasvOnly <tejasvonly@gmail.com>
2024-12-14 01:27:35 -0800

    chore: update trc20 list and add migration

---
+++ packages/core-services/src/db/migrations/items/006.trc20Token/idChanges.ts
@@ -0,0 +1,18 @@
+const data = {
+  idChanges: [
+    {
+      oldId: 'wrapped-btt',
+      newId: 'wrapped-btt-tron',
+      platform: 'tron',
+    },
+  ],
+  changedCoins: [
+    {
+      id: 'wrapped-btt-tron',
+      platform: 'tron',
+    },
+  ],
+};
+
+export const { idChanges } = data;
+export const { changedCoins } = data;
---
+++ packages/core-services/src/db/migrations/items/006.trc20Token/index.ts
@@ -0,0 +1,23 @@
+import { changedCoins, idChanges } from './idChanges';
+
+import {
+  migrateTokenDetailsChangeInDb,
+  migrateTokenIdChangeInDb,
+  resetContractTransactionBlockHeight,
+} from '../../helpers';
+import { IMigrationItem } from '../../types';
+
+const migration: IMigrationItem = {
+  id: '5',
+  name: 'ERC20 token list update migration',
+  up: async db => {
+    await migrateTokenIdChangeInDb(db, idChanges);
+    await migrateTokenDetailsChangeInDb(db, changedCoins);
+    await resetContractTransactionBlockHeight(db, migration.name);
+  },
+  down: async () => {
+    // Not required as of this migration
+  },
+};
+
+export default migration;
