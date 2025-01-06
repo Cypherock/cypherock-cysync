1cd1e08ca9e014a9f35541fc9c0439a8ccbf25dd
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-01-06 14:02:52 +0530

    fix: ci

--- packages/coin-support-starknet/src/utils/completeHexHash.ts
+++ packages/coin-support-starknet/src/utils/completeHexHash.ts
@@ -1,5 +1,4 @@
 const removeHexPrefix = (hex: string) => hex.replace(/^0x/i, '');
 
-export const completeHexHash = (input: string, maxLength = 64) => {
-  return "0x" + removeHexPrefix(input).padStart(maxLength, "0");
-};
+export const completeHexHash = (input: string, maxLength = 64) =>
+  `0x${removeHexPrefix(input).padStart(maxLength, '0')}`;
