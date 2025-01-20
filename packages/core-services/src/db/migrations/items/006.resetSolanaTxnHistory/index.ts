6a0d8f38237708b4a0d9fb225b10913ea69038cb
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-01-20 12:00:29 +0530

    feat: added reset solana txn history migration

---
+++ packages/core-services/src/db/migrations/items/006.resetSolanaTxnHistory/index.ts
@@ -0,0 +1,38 @@
+import { IMigrationItem } from '../../types';
+import { coinFamiliesMap } from '@cypherock/coins';
+
+/**
+ * Reset solana transaction history migration
+ * - Reset solana transaction history to sync token accounts previously transacted with
+ */
+const migration: IMigrationItem = {
+  id: '6',
+  name: 'Reset Solana transaction history',
+  up: async db => {
+    const allSolanaTransactions = await db.transaction.remove({
+      familyId: coinFamiliesMap.solana,
+    });
+
+    const transactionIdUpdateDetails = [
+      {
+        repository: db.transactionNotificationRead,
+        key: 'transactionId',
+      },
+      {
+        repository: db.transactionNotificationClick,
+        key: 'transactionId',
+      },
+    ];
+
+    for (const { repository, key } of transactionIdUpdateDetails) {
+      for (const transaction of allSolanaTransactions) {
+        await repository.remove({ [key]: transaction.__id });
+      }
+    }
+  },
+  down: async () => {
+    // Not required as of this migration
+  },
+};
+
+export default migration;
