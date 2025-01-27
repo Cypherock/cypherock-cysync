dd237d01a3fa32cbcf79448d9e64bd0a3da325ce
AryanCypherock <aryan@cypherock.com>
2025-01-27 17:32:25 +0530

    feat: updated app ui

--- apps/mobile/app/(onboarding)/_layout.tsx
+++ apps/mobile/app/(onboarding)/_layout.tsx
@@ -1,27 +1,12 @@
-import { keyValueStore } from '@/db';
-import { Redirect, Stack } from 'expo-router';
-import { useEffect, useState } from 'react';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const onBoardingFinished =
-        await keyValueStore.isOnboardingCompleted.get();
-      setIsOnboardingCompleted(onBoardingFinished);
-    })();
-  }, []);
-
-  if (isOnboardingCompleted) {
-    return <Redirect href={'/(tabs)/history'} />;
-  }
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="info" />
       <Stack.Screen name="onboarding" />
+      <Stack.Screen name="PasswordPage" />
       <Stack.Screen name="permission" />
     </Stack>
   );
--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,21 +1,30 @@
-import { getDefaultTheme } from '@/components/ui/themes';
-import { Slot, Stack } from 'expo-router';
+import { getDefaultTheme, getLargeTheme } from '@/components/ui';
+import { Stack } from 'expo-router';
 import { ThemeProvider } from 'styled-components/native';
 import * as SystemUI from 'expo-system-ui';
 import { colors } from '@/components/ui/themes/color.styled';
 import { store, StoreProvider } from '@/store';
 import { CustomRealmProvider } from '@/db';
-import { BgTasks } from '@/components/core/BgTasks';
-import React from 'react';
+import { BgTasks } from '@/components/core';
+import React, { useEffect, useState } from 'react';
+import { useWindowDimensions } from 'react-native';
 
 SystemUI.setBackgroundColorAsync(colors.background.primary);
 
 export default function RootLayout() {
-  const theme = getDefaultTheme();
+  const { width } = useWindowDimensions();
+  const [currentTheme, setCurrentTheme] = useState(getDefaultTheme());
+
+  useEffect(() => {
+    const largeScreenThreshold = 330;
+    setCurrentTheme(
+      width >= largeScreenThreshold ? getLargeTheme() : getDefaultTheme(),
+    );
+  }, [width]);
 
   return (
     <StoreProvider store={store}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={currentTheme}>
         <CustomRealmProvider>
           <Stack
             screenOptions={{
@@ -25,7 +34,7 @@ export default function RootLayout() {
           >
             <Stack.Screen name="(onboarding)" />
             <Stack.Screen name="(tabs)" />
-            <Slot />
+            <Stack.Screen name="scan" />
           </Stack>
           <BgTasks />
         </CustomRealmProvider>
