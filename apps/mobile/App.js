3ff295bf5328896e2082f78a902a60bad16c4c1a
AryanCypherock <aryan@cypherock.com>
2025-01-27 22:23:01 +0530

    fix: pollyfill fixes in receive flow

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,3 +1,18 @@
-require('node-libs-react-native/globals');
 import 'react-native-get-random-values';
+import 'node-libs-react-native/globals';
+import { randomBytes } from 'react-native-randombytes';
+
+if (__DEV__) {
+  process.env = { ...process.env, NODE_ENV: 'development' };
+} else {
+  process.env = { ...process.env, NODE_ENV: 'production' };
+}
+
+// polyfill for random bytes
+if (!global.crypto) {
+  global.crypto = {
+    getRandomValues: buffer => randomBytes(buffer.length).copy(buffer),
+  };
+}
+
 import 'expo-router/entry';
