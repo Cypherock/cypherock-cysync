ad60cd3d14dc7bec7c385c759fbb9910a19537bc
AryanCypherock <aryan@cypherock.com>
2025-01-28 18:57:28 +0530

    fix: onboarding bug and prevent splash screen hide

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,16 +4,40 @@ import { ThemeProvider } from 'styled-components/native';
 import * as SystemUI from 'expo-system-ui';
 import { colors } from '@/components/ui/themes/color.styled';
 import { store, StoreProvider } from '@/store';
-import { CustomRealmProvider } from '@/db';
+import { CustomRealmProvider, keyValueStore } from '@/db';
 import { BgTasks } from '@/components/core';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useWindowDimensions } from 'react-native';
+import { SplashScreen } from 'expo-router';
 
 SystemUI.setBackgroundColorAsync(colors.background.primary);
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const { width } = useWindowDimensions();
   const [currentTheme, setCurrentTheme] = useState(getDefaultTheme());
+  const [onBoaridngCompleted, setOnBoardingCompleted] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  async function getOnboardingStatus() {
+    const onBoardingStatus = await keyValueStore.isOnboardingCompleted.get();
+    setOnBoardingCompleted(onBoardingStatus);
+  }
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getOnboardingStatus();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        SplashScreen.hide();
+      }
+    }
+
+    prepare();
+  }, []);
 
   useEffect(() => {
     const largeScreenThreshold = 330;
@@ -21,6 +45,10 @@ export default function RootLayout() {
       width >= largeScreenThreshold ? getLargeTheme() : getDefaultTheme(),
     );
   }, [width]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
     <StoreProvider store={store}>
@@ -32,8 +60,9 @@ export default function RootLayout() {
               contentStyle: { backgroundColor: colors.background.primary },
             }}
           >
-            <Stack.Screen name="(onboarding)" />
+            <Stack.Screen name="(onboarding)" redirect={onBoaridngCompleted} />
             <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="permission" />
             <Stack.Screen name="scan" />
           </Stack>
           <BgTasks />
