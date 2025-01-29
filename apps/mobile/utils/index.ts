4b26fd4093f8e8aa3f14bb09bb8d8e52cca1c422
AryanCypherock <aryan@cypherock.com>
2025-01-29 16:37:55 +0530

    feat: search accounts implemented

---
+++ apps/mobile/utils/index.ts
@@ -0,0 +1,36 @@
+import { IInteractiveItemListItem } from '@/components/ui';
+
+export const findSelectedItem = (
+  menuItems: IInteractiveItemListItem[],
+  selectedId: string | undefined,
+): IInteractiveItemListItem | undefined =>
+  menuItems.find(item => item.id === selectedId);
+
+export const searchInItems = (
+  menuItems: IInteractiveItemListItem[],
+  searchString: string,
+): IInteractiveItemListItem[] => {
+  const filteredItems: IInteractiveItemListItem[] = [];
+
+  for (const item of menuItems) {
+    const shouldAdd = (
+      item.text +
+      (item.altText ?? '') +
+      (item.rightText ?? '') +
+      (item.tag ?? '')
+    )
+      .toLowerCase()
+      .includes(searchString.toLowerCase());
+
+    if (shouldAdd) {
+      if (item.$parentId) {
+        const parentItem = findSelectedItem(menuItems, item.$parentId);
+        if (parentItem && !findSelectedItem(filteredItems, parentItem.id)) {
+          filteredItems.push(parentItem);
+        }
+      }
+      filteredItems.push(item);
+    }
+  }
+  return filteredItems;
+};
