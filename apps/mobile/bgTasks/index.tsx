4081e4203ecbb7751805960a89f872da7bf35809
AryanCypherock <aryan@cypherock.com>
2025-02-08 02:55:14 +0530

    feat: account sync added

--- apps/mobile/bgTasks/index.tsx
+++ apps/mobile/bgTasks/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AccountSyncTask } from './accountsSync';
 import { DatabaseListener } from './dbSync';
-
-export const BackgroundTasks = () => (
-  <>
-    <AccountSyncTask />
-    <DatabaseListener />
-  </>
-);
+let render = 0;
+export const BackgroundTasks = () => {
+  useEffect(() => {
+    console.log(`background task rendered ${render++} times`);
+  }, []);
+  return (
+    <>
+      <AccountSyncTask />
+      <DatabaseListener />
+    </>
+  );
+};
