133f45bf99eb1be2bab013a81454b74a5d3e01f8
AryanCypherock <aryan@cypherock.com>
2025-02-08 02:49:58 +0530

    feat: dependencies added for account sync

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,6 +1,8 @@
 import 'react-native-get-random-values';
 import 'node-libs-react-native/globals';
 import { randomBytes } from 'react-native-randombytes';
+import structuredClone from '@ungap/structured-clone';
+import { setGlobalDependencies } from './utils';
 
 if (__DEV__) {
   process.env = { ...process.env, NODE_ENV: 'development' };
@@ -14,5 +16,24 @@ if (!global.crypto) {
     getRandomValues: buffer => randomBytes(buffer.length).copy(buffer),
   };
 }
+
+// plyfill for structuredClone (required in core services)
+if (!('structuredClone' in globalThis)) {
+  globalThis.structuredClone = structuredClone;
+}
+
+// polyfills for coin support
+globalThis.BitcoinJsLib = require('bitcoinjs-lib');
+globalThis.NearApiJs = require('near-api-js');
+globalThis.eip712 = require('eip-712');
+globalThis.ethers = require('ethers');
+globalThis.TronWeb = require('tronweb');
+globalThis.web3 = require('web3');
+globalThis.solanaWeb3 = require('@solana/web3.js');
+globalThis.solanaSplToken = require('@solana/spl-token');
+globalThis.xrpl = require('xrpl');
+globalThis.starknet = require('starknet');
+
+setGlobalDependencies();
 
 import 'expo-router/entry';
---
+++ apps/mobile/utils/dependencies.ts
@@ -0,0 +1,22 @@
+import { BtcSupport } from '@cypherock/coin-support-btc';
+import { EvmSupport } from '@cypherock/coin-support-evm';
+import { NearSupport } from '@cypherock/coin-support-near';
+import { SolanaSupport } from '@cypherock/coin-support-solana';
+import { StarknetSupport } from '@cypherock/coin-support-starknet';
+import { TronSupport } from '@cypherock/coin-support-tron';
+import { XrpSupport } from '@cypherock/coin-support-xrp';
+
+export const setGlobalDependencies = () => {
+  BtcSupport.setBitcoinLibrary((globalThis as any).BitcoinJsLib);
+  NearSupport.setNearApiJs((globalThis as any).NearApiJs);
+  EvmSupport.setEthersLibrary((globalThis as any).ethers);
+  EvmSupport.setEip712Library((globalThis as any).eip712);
+  EvmSupport.setWeb3Library((globalThis as any).web3);
+  SolanaSupport.setWeb3Library((globalThis as any).solanaWeb3);
+  SolanaSupport.setSplTokenLibrary((globalThis as any).solanaSplToken);
+  TronSupport.setTronWeb(
+    new (globalThis as any).TronWeb({ fullHost: 'https://api.trongrid.io' }),
+  );
+  XrpSupport.setXrpLib((globalThis as any).xrpl);
+  StarknetSupport.setStarknetLib((globalThis as any).starknet);
+};
