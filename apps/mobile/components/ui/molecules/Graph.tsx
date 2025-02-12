fee81b68253ca11a1a1f5085d9f3b22db1df170c
AryanCypherock <aryan@cypherock.com>
2025-02-12 18:04:21 +0530

    feat: added ui components

--- apps/mobile/components/ui/molecules/Graph.tsx
+++ apps/mobile/components/ui/molecules/Graph.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useRef, useState } from 'react';
 import { LineChart, LineChartPropsType } from 'react-native-gifted-charts';
 import { colors } from '../themes/color.styled';
 import { Dimensions } from 'react-native';
@@ -29,12 +29,38 @@ export const DataPointLabel = styled.Text`
   background: ${({ theme }) => theme.palette.background.secondary};
 `;
 
-export const Graph: FC<LineChartPropsType> = ({ ...props }) => {
+const hexToRGB = (value: string) => {
+  const numericValue = parseInt(value.replace('#', ''), 16);
+  // eslint-disable-next-line no-bitwise
+  const r = (numericValue >> 16) & 0xff;
+  // eslint-disable-next-line no-bitwise
+  const g = (numericValue >> 8) & 0xff;
+  // eslint-disable-next-line no-bitwise
+  const b = numericValue & 0xff;
+  return { r, g, b };
+};
+
+export const DisplayGraph: FC<LineChartPropsType> = ({ ...props }) => {
+  const { current: colorRgb } = useRef(hexToRGB(props.color ?? 'E9B873'));
+  const [labelShift, setLabelShift] = useState(0);
+
+  const handleFocus = (_: any, x: number) => {
+    if (!props.data) return;
+    const shiftAmount = 150;
+    if (x > props.data?.length / 2.4) {
+      setLabelShift(-shiftAmount);
+    } else {
+      setLabelShift(0);
+    }
+  };
+
   return (
     <LineChart
       {...props}
       focusEnabled
       showStripOnFocus
+      initialSpacing={1}
+      endSpacing={1}
       stripHeight={156}
       stripStrokeDashArray={[5, 2]}
       showDataPointOnFocus
@@ -43,20 +69,25 @@ export const Graph: FC<LineChartPropsType> = ({ ...props }) => {
       focusedCustomDataPoint={() => <DataPointIcon />}
       showDataPointLabelOnFocus
       curved
-      width={width - 80}
+      width={width - 48}
       height={156}
       textColor={'rgba(139, 134, 130, 1)'}
       rulesColor={colors.text.secondary}
       xAxisColor={colors.text.secondary}
       yAxisColor={'transparent'}
       dataPointsColor="transparent"
-      color1="#E9B873"
-      startFillColor="#FFAC0A"
+      color1={props.color}
+      startFillColor={`rgba(${colorRgb.r}, ${colorRgb.g}, ${colorRgb.b}, 1)`}
       startOpacity={1}
-      endFillColor="rgba(40, 37, 37, 0.00)"
+      endFillColor={`rgba(${colorRgb.r}, ${colorRgb.g}, ${colorRgb.b}, 0)`}
       endOpacity={0}
       yAxisTextStyle={{ color: colors.text.secondary, fontSize: 10 }}
-      xAxisLabelTextStyle={{ color: colors.text.secondary, fontSize: 10 }}
+      xAxisLabelTextStyle={{ opacity: 0 }}
+      disableScroll
+      dataPointLabelShiftY={20}
+      dataPointLabelShiftX={labelShift}
+      onFocus={handleFocus}
+      delayBeforeUnFocus={1000}
     />
   );
 };
