0063391c1e2ceb1217642a4289f9b1c05cb81a13
AryanCypherock <aryan@cypherock.com>
2025-02-22 20:56:05 +0530

    refactor: updated lang strings

--- apps/mobile/constants/i18n/types.ts
+++ apps/mobile/constants/i18n/types.ts
@@ -190,8 +190,8 @@ export interface LangSupport {
 export interface LangSettings {
   heading: string;
   settings: {
-    general: string;
-    app: string;
+    displayLanguage: string;
+    password: string;
     about: string;
   };
   // general
@@ -216,7 +216,7 @@ export interface LangSettings {
     privacyPolicy: string;
   };
   // app
-  app: {
+  password: {
     heading: string;
     addNewPassword: string;
     changePassword: string;
