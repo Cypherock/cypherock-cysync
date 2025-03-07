c57a6cd1738b5fef93d740c05aeedf9f00e5d2cd
AryanCypherock <aryan@cypherock.com>
2025-03-07 13:12:22 +0530

    fix: navigation bug in onboarding

--- apps/mobile/app/(app)/(protected)/(onboarding)/_layout.tsx
+++ apps/mobile/app/(app)/(protected)/(onboarding)/_layout.tsx
@@ -1,11 +1,14 @@
+import { useLockscreen } from '@/contexts/useLockscreenContext';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
+  const { isPasswordSet } = useLockscreen();
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="onboarding" />
-      <Stack.Screen name="password-page" />
+      <Stack.Screen name="password-page" redirect={isPasswordSet} />
       <Stack.Screen name="onboarding-scan" />
       <Stack.Screen name="sync" />
     </Stack>
