c2e5564864bfbcb4153ded7ed6853d02f33ccc12
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-03-25 23:18:47 +0530

    feat: added icp token support lib

--- packages/coin-support-icp/src/utils/dfinityLib.ts
+++ packages/coin-support-icp/src/utils/dfinityLib.ts
@@ -1,11 +1,13 @@
 import type agent from '@dfinity/agent';
 import type candid from '@dfinity/candid';
 import type icp from '@dfinity/ledger-icp';
+import type icrc from '@dfinity/ledger-icrc';
 import type principal from '@dfinity/principal';
 
 export interface DfinityLib {
   agent: typeof agent;
   icp: typeof icp;
+  icrc: typeof icrc;
   candid: typeof candid;
   principal: typeof principal;
 }
