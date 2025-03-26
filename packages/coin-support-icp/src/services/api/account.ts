b2ae98394253bb383c35edb3afbd5d964900136f
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-03-26 11:07:30 +0530

    feat: added icp getTokenBalance api call

--- packages/coin-support-icp/src/services/api/account.ts
+++ packages/coin-support-icp/src/services/api/account.ts
@@ -1,12 +1,13 @@
 import { getCoinSupportDfinityLib } from '../../utils';
 
 const ICP_LEDGER_CANISTER_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai';
+const HOST = 'https://icp-api.io';
 
 export const getBalance = async (accountId: string) => {
   try {
     const { agent, icp, principal } = getCoinSupportDfinityLib();
     const ledger = icp.LedgerCanister.create({
-      agent: await agent.HttpAgent.create({ host: 'https://icp-api.io' }),
+      agent: await agent.HttpAgent.create({ host: HOST }),
       canisterId: principal.Principal.fromText(ICP_LEDGER_CANISTER_ID),
     });
 
@@ -17,5 +18,27 @@ export const getBalance = async (accountId: string) => {
     return balance.toString();
   } catch (error) {
     throw new Error('Error fetching ICP account balance');
+  }
+};
+
+export const getTokenBalance = async (
+  principalID: Uint8Array,
+  tokenLedgerCanisterId: string,
+) => {
+  try {
+    const { agent, icrc, principal } = getCoinSupportDfinityLib();
+
+    const icrcLedger = icrc.IcrcLedgerCanister.create({
+      agent: await agent.HttpAgent.create({ host: HOST }),
+      canisterId: principal.Principal.from(tokenLedgerCanisterId),
+    });
+
+    const balance = await icrcLedger.balance({
+      owner: principal.Principal.from(principalID),
+    });
+
+    return balance.toString();
+  } catch (error) {
+    throw new Error('Error fetching ICP token account balance');
   }
 };
