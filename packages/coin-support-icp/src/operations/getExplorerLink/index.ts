1401f6129b258d2534adcd1d3bfcaef286b1ad40
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-03-29 23:26:15 +0530

    fix: icp token txn explorer link

--- packages/coin-support-icp/src/operations/getExplorerLink/index.ts
+++ packages/coin-support-icp/src/operations/getExplorerLink/index.ts
@@ -1,4 +1,13 @@
 import { IGetExplorerLink } from '@cypherock/coin-support-interfaces';
+import { icpCoinList, IcpIdMap } from '@cypherock/coins';
 
-export const getExplorerLink = (params: IGetExplorerLink) =>
-  `https://dashboard.internetcomputer.org/transaction/${params.transaction.hash}`;
+export const getExplorerLink = (params: IGetExplorerLink) => {
+  const { transaction } = params;
+
+  if (transaction.assetId === IcpIdMap.icp) {
+    return `https://dashboard.internetcomputer.org/transaction/${transaction.hash}`;
+  }
+  const tokenDetails =
+    icpCoinList[transaction.parentAssetId].tokens[transaction.assetId];
+  return `https://dashboard.internetcomputer.org/${tokenDetails.dashboardRoute}/transaction/${transaction.hash}`;
+};
