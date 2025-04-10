24fbd15c02ff4ba596c88bfd655846558d23fa16
AryanCypherock <aryan@cypherock.com>
2025-04-10 17:43:20 +0530

    fix: updated splash screen

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,42 +1,63 @@
-import { getDefaultTheme, getLargeTheme, LottieSplash } from '@/components/ui';
+import {
+  getDefaultTheme,
+  getLargeTheme,
+  LottieSplash,
+  ThemeType,
+} from '@/components/ui';
 import { Slot } from 'expo-router';
 import { ThemeProvider } from 'styled-components/native';
 import * as SystemUI from 'expo-system-ui';
 import { colors } from '@/components/ui/themes/color.styled';
 import { store, StoreProvider } from '@/store';
-import React, { useEffect, useState } from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { useWindowDimensions } from 'react-native';
 import { SplashScreen } from 'expo-router';
-import { LockscreenProvider } from '@/contexts/useLockscreenContext';
+import {
+  LockscreenProvider,
+  useLockscreen,
+} from '@/contexts/useLockscreenContext';
 import { CustomRealmProvider } from '@/db';
 
 SplashScreen.preventAutoHideAsync();
 SystemUI.setBackgroundColorAsync(colors.background.primary);
 
 export default function RootLayout() {
-  const { width } = useWindowDimensions();
   const [currentTheme, setCurrentTheme] = useState(getDefaultTheme());
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    const largeScreenThreshold = 330;
-    setCurrentTheme(
-      width >= largeScreenThreshold ? getLargeTheme() : getDefaultTheme(),
-    );
-    setAppIsReady(true);
-  }, [width]);
-
-  if (!appIsReady) return null;
-
   return (
     <StoreProvider store={store}>
       <ThemeProvider theme={currentTheme}>
         <CustomRealmProvider>
           <LockscreenProvider>
-            <Slot />
+            <AnimatedSplashScreen setTheme={setCurrentTheme}>
+              <Slot />
+            </AnimatedSplashScreen>
           </LockscreenProvider>
         </CustomRealmProvider>
       </ThemeProvider>
     </StoreProvider>
   );
+}
+
+function AnimatedSplashScreen({
+  children,
+  setTheme,
+}: { setTheme: (theme: ThemeType) => void } & PropsWithChildren) {
+  const { width } = useWindowDimensions();
+  const { isLockscreenLoading } = useLockscreen();
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const largeScreenThreshold = 330;
+    setTheme(
+      width >= largeScreenThreshold ? getLargeTheme() : getDefaultTheme(),
+    );
+    setAppIsReady(true);
+  }, [width]);
+
+  if (!appIsReady || isLockscreenLoading) {
+    SplashScreen.hide();
+    return <LottieSplash source={require('@/assets/lottie/splash.json')} />;
+  }
+
+  return children;
 }
