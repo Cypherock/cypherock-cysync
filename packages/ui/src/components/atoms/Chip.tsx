7e2b72903796dc253b4c86d3c7c6ea4ac280452a
AryanCypherock <aryan@cypherock.com>
2025-05-06 17:36:52 +0530

    feat: chip updated for different gradient variations

--- packages/ui/src/components/atoms/Chip.tsx
+++ packages/ui/src/components/atoms/Chip.tsx
@@ -7,12 +7,20 @@ import {
   font,
   silverGradient,
   spacing,
+  goldenGradient,
 } from '../utils';
+
+type GradientType = 'silver' | 'golden';
 
 interface ChipProps extends SpacingProps, FontProps {
   children: React.ReactNode;
-  $gradient?: boolean;
+  $gradient?: GradientType;
 }
+
+const gradientMap: Record<GradientType, any> = {
+  silver: silverGradient('background'),
+  golden: goldenGradient('background'),
+};
 
 const ChipStyle = styled.div<ChipProps>`
   display: flex;
@@ -20,10 +28,12 @@ const ChipStyle = styled.div<ChipProps>`
   gap: var(--0-px, 0px);
   align-items: flex-start;
   border-radius: 29px;
-  border: 1px solid ${({ theme }) => theme.palette.border.muted};
+  border: 1px solid
+    ${({ theme, $gradient }) =>
+      $gradient !== 'golden' && theme.palette.border.muted};
   ${spacing}
   ${font}
-  ${({ $gradient }) => ($gradient ? silverGradient('background') : '')}
+  ${({ $gradient }) => $gradient && gradientMap[$gradient]}
 `;
 
 export const Chip: FC<ChipProps> = ({ children, ...props }) => (
