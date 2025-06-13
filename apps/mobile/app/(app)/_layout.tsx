76136ec470d1f17b6c8058c6e5742304cb2c0852
AryanCypherock <aryan@cypherock.com>
2025-06-13 18:06:46 +0530

    feat: error boundary added

--- apps/mobile/app/(app)/_layout.tsx
+++ apps/mobile/app/(app)/_layout.tsx
@@ -1,7 +1,16 @@
+import { ErrorScreen } from '@/components/core';
 import { useTheme } from '@/components/ui';
 import { useLockscreen } from '@/contexts/useLockscreenContext';
-import { Stack } from 'expo-router';
-import React from 'react';
+import logger from '@/utils/logger';
+import { ErrorBoundaryProps, Stack } from 'expo-router';
+import React, { useEffect } from 'react';
+
+export function ErrorBoundary({ error }: ErrorBoundaryProps) {
+  useEffect(() => {
+    logger.error(error.message, error);
+  }, [error]);
+  return <ErrorScreen message={error.message} />;
+}
 
 export default function Layout() {
   const { isLocked, isPasswordSet } = useLockscreen();
