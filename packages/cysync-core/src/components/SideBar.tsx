b0559a030d345e7c0eeb93f75c643e2bd03b8dc3
AryanCypherock <aryan@cypherock.com>
2025-07-15 21:18:21 +0530

    fix: color scheme changes and ui updates

--- packages/cysync-core/src/components/SideBar.tsx
+++ packages/cysync-core/src/components/SideBar.tsx
@@ -249,15 +249,7 @@ const SideBarComponent: FC = () => {
             )}
           </Flex>
         </Flex>
-        <Flex
-          align="center"
-          justify="flex-start"
-          gap={8}
-          $borderWidthT={1}
-          $borderColor="separator"
-          pt="16px"
-          mt="16px"
-        >
+        <Flex align="center" justify="flex-start" gap={8} pt="16px" mt="16px">
           <Image src={cysyncLogoSmallImage} alt="Cypherock" $height={20} />
           <Typography
             variant="h6"
@@ -269,7 +261,9 @@ const SideBarComponent: FC = () => {
           </Typography>
         </Flex>
       </SideBarWrapper>
-      <SidebarHandle onMouseDown={startDrag} />
+      {window.cysyncEnv.VENDOR === 'default' && (
+        <SidebarHandle onMouseDown={startDrag} />
+      )}
     </>
   );
 };
