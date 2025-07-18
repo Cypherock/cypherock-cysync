06cf0833f4064fb82b772739ce4fae9881f4230d
AryanCypherock <aryan@cypherock.com>
2025-07-18 14:30:28 +0530

    feat: icons updated

--- apps/desktop/electron-builder.js
+++ apps/desktop/electron-builder.js
@@ -11,6 +11,10 @@ const getArtifactName = (withoutArch = false) => {
   return `${productNameInArtifact}-\${version}-\${platform}-\${arch}.\${ext}`;
 };
 
+const BUILD_RESOURCES_PATH = process.env.VENDOR
+  ? 'build/' + process.env.VENDOR
+  : 'build/default';
+
 const config = {
   // TODO: Needs to be updated according to the vendor
   appId: 'com.hodl.cypherock',
@@ -19,7 +23,7 @@ const config = {
   asarUnpack: ['!**/*.node'],
   directories: {
     output: 'release/${version}',
-    buildResources: 'build',
+    buildResources: BUILD_RESOURCES_PATH,
   },
   files: [
     'dist-electron',
@@ -64,7 +68,7 @@ const config = {
   linux: {
     target: ['AppImage'],
     category: 'Utility',
-    executableName: 'Cypherock CySync',
+    executableName: productName,
     artifactName: getArtifactName(),
   },
   nsis: {
