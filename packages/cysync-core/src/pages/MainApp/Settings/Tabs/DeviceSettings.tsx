a285ccaa246ddab52cd41cb69eb4c1b5b68b14c7
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-07-18 23:05:59 +0530

    feat: added switch firmware option in device settings

--- packages/cysync-core/src/pages/MainApp/Settings/Tabs/DeviceSettings.tsx
+++ packages/cysync-core/src/pages/MainApp/Settings/Tabs/DeviceSettings.tsx
@@ -1,20 +1,50 @@
 import { LangDisplay } from '@cypherock/cysync-ui';
+import { createSelector } from '@reduxjs/toolkit';
 import React from 'react';
 
 import {
   openAuthenticateX1CardDialog,
   openAuthenticateX1VaultDialog,
+  openSwitchFirmwareDialog,
 } from '~/actions';
-import { selectLanguage, useAppDispatch, useAppSelector } from '~/store';
+import {
+  selectLanguage,
+  selectLastConnectedFirmware,
+  useAppDispatch,
+  useAppSelector,
+} from '~/store';
 
 import { SettingsButton, SettingsStandardItem } from '../components';
 
+const selector = createSelector(
+  [selectLanguage, selectLastConnectedFirmware],
+  ({ strings }, { isFirmwareBtcOnly }) => ({
+    strings,
+    isFirmwareBtcOnly,
+  }),
+);
+
 export const DeviceSettings: React.FC = () => {
-  const { strings } = useAppSelector(selectLanguage);
+  const { strings, isFirmwareBtcOnly } = useAppSelector(selector);
   const { item } = strings.settings.tabs.device;
   const dispatch = useAppDispatch();
   return (
     <>
+      {!isFirmwareBtcOnly && (
+        <SettingsStandardItem
+          title={{ text: 'Switch firmware to Bitcoin-only' }}
+          description={{
+            text: 'Bitcoin-only firmware only works with Bitcoin transactions.\nOnce switched to Bicoin-only firmware, you can nt switch back to multi-coin firmware',
+          }}
+        >
+          <SettingsButton
+            onClick={() => dispatch(openSwitchFirmwareDialog())}
+            variant="primary"
+          >
+            <LangDisplay text="Switch firmware" />
+          </SettingsButton>
+        </SettingsStandardItem>
+      )}
       <SettingsStandardItem
         title={{ text: item.x1VaultAuth.title }}
         description={{ text: item.x1VaultAuth.description }}
