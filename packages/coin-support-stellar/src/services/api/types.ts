dc3435cd3d48d710b13ba96d1b3b22b43d599df1
Keyur279 <keyur@cypherock.com>
2025-08-06 17:40:39 +0530

    feat: added services for stellar

---
+++ packages/coin-support-stellar/src/services/api/types.ts
@@ -0,0 +1,57 @@
+export interface IStellarTransactionParams {
+  address: string;
+  assetId: string;
+  limit?: number;
+  forward?: boolean;
+  binary?: boolean;
+  ledgerIndexMin?: number;
+}
+
+export enum StellarMemoType {
+  NONE = 'none',
+  TEXT = 'text',
+  ID = 'id',
+  HASH = 'hash',
+  RETURN = 'return',
+}
+
+export interface IStellarMemo {
+  type: StellarMemoType;
+  value?: string; // Will store text, ID (as string), or hash (as hex string)
+}
+
+interface IStellarResponseTransaction {
+  hash: string;
+  source_account: string;
+  fee_charged: string;
+  ledger: number;
+  memo_type?: string;
+  memo?: string;
+  operations: {
+    type: string;
+    from: string;
+    to: string;
+    amount: string;
+  }[];
+  sequence: number;
+  date: number;
+}
+
+export interface IDetailedStellarResponseTransaction {
+  memo_type?: string;
+  memo?: string;
+  meta: {
+    TransactionResult: string;
+    delivered_amount: string;
+  };
+  tx: IStellarResponseTransaction;
+}
+
+export interface IStellarTransactionResult {
+  account: string;
+  transactions: IDetailedStellarResponseTransaction[];
+  limit: number;
+  marker: {
+    ledger: number;
+  };
+}
