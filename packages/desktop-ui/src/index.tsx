45ac7e311bc469d3033d48debb3f4e2f9322d73b
AryanCypherock <aryan@cypherock.com>
2025-09-10 14:38:34 +0530

    feat: page view events added for mixpanel and service init

--- packages/desktop-ui/src/index.tsx
+++ packages/desktop-ui/src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './app';
 import { setupCoreDependencies } from './utils/setupCore';
+import { analyticsService } from '@cypherock/cysync-core';
 
 export async function render(target?: HTMLElement) {
   await setupCoreDependencies();
+  analyticsService.init();
 
   const root = ReactDOM.createRoot(target ?? document.body);
   root.render(<App />);
