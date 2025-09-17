db4071cac2a2c5c555f89ad84d53b45208800785
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-09-17 13:34:18 +0530

    feat: latest firmware version provider as per variant

--- packages/cysync-core/src/context/latestDeviceVersion.tsx
+++ packages/cysync-core/src/context/latestDeviceVersion.tsx
@@ -1,10 +1,16 @@
-import { ManagerApp } from '@cypherock/sdk-app-manager';
-import React, { useCallback, useEffect, useMemo } from 'react';
+import { FirmwareVariant, ManagerApp } from '@cypherock/sdk-app-manager';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import semver from 'semver';
 
 import { useStateToRef } from '~/hooks';
+import logger from '~/utils/logger';
 
-import { DeviceConnectionStatus, useDevice } from '..';
+import {
+  DeviceConnectionStatus,
+  selectLastConnectedFirmware,
+  useAppSelector,
+  useDevice,
+} from '..';
 
 export interface LatestDeviceVersionContextInterface {
   version: string | undefined;
@@ -24,10 +30,17 @@ export const LatestDeviceVersionProvider: React.FC<
 > = ({ children }) => {
   const { connection: connectionInfo } = useDevice();
   const [version, setVersion] = React.useState<string | undefined>();
+  const requestIdRef = useRef<number>(0);
 
   const connectionRef = useStateToRef({ connectionInfo });
 
+  const { isFirmwareBtcOnly } = useAppSelector(selectLastConnectedFirmware);
+  const variant = isFirmwareBtcOnly
+    ? FirmwareVariant.BTC_ONLY
+    : FirmwareVariant.MULTI_COIN;
+
   const fetchLatestVersion = useCallback(async () => {
+    const currentRequestId = requestIdRef.current + 1;
     setVersion(undefined);
     const connection = connectionRef.current.connectionInfo;
 
@@ -35,21 +48,32 @@ export const LatestDeviceVersionProvider: React.FC<
       !connection ||
       connection.status !== DeviceConnectionStatus.CONNECTED ||
       connection.isBootloader
-    )
+    ) {
       return;
+    }
 
     const result = await ManagerApp.getLatestFirmware({
       prerelease: window.cysyncEnv.ALLOW_PRERELEASE === 'true',
+      variant,
     });
+
+    if (currentRequestId !== requestIdRef.current) {
+      logger.info('Request is outdated, ignoring result', {
+        currentRequestId,
+        latestRequestId: requestIdRef.current,
+      });
+      return;
+    }
 
     if (
       connection.firmwareVersion &&
       semver.gte(connection.firmwareVersion, result.version)
-    )
+    ) {
       return;
+    }
 
     setVersion(result.version);
-  }, []);
+  }, [variant]);
 
   useEffect(() => {
     const minutes = 15;
@@ -62,7 +86,7 @@ export const LatestDeviceVersionProvider: React.FC<
 
   useEffect(() => {
     fetchLatestVersion();
-  }, [connectionInfo]);
+  }, [connectionInfo, variant]);
 
   const ctx = useMemo(
     () => ({
