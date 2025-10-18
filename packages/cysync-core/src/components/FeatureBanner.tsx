2f873bc106b1cd4e09d115774ef837d3178c1614
AryanCypherock <aryan@cypherock.com>
2025-10-18 16:47:09 +0530

    feat: added canton banner

--- packages/cysync-core/src/components/FeatureBanner.tsx
+++ packages/cysync-core/src/components/FeatureBanner.tsx
@@ -3,56 +3,84 @@ import {
   Typography,
   Button,
   Image,
-  cysyncLogoSmall,
+  cantonIcon,
+  LangDisplay,
 } from '@cypherock/cysync-ui';
-import React, { FC, useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
-
-import { routes } from '~/constants';
-import { useNavigateTo } from '~/hooks';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { selectLanguage, useAppSelector } from '~/store';
+import { keyValueStore } from '~/utils';
 
 export const FeatureBanner: FC = () => {
+  const [isCantonAdded, setIsCantonAdded] = useState(false);
+  const [isAutomaticApprovalEnabled, setIsAutomaticApprovalEnabled] =
+    useState(false);
   const lang = useAppSelector(selectLanguage);
-  const location = useLocation();
-  const navigateTo = useNavigateTo();
+  const strings = lang.strings.portfolio.banner;
 
-  const { path } = routes.portfolio;
+  useEffect(() => {
+    keyValueStore.isCantonAdded.get().then(value => {
+      setIsCantonAdded(value);
+    });
+    keyValueStore.isAutomaticApprovalsEnabled.get().then(value => {
+      setIsAutomaticApprovalEnabled(value);
+    });
+  }, []);
 
-  if (!location.pathname.startsWith(path)) {
-    return null;
-  }
+  const enableAutomaticApprovals = useCallback(() => {
+    // TODO: Implement the logic to enable automatic approvals for canton
+    setIsCantonAdded(true);
+  }, []);
 
-  const toCypherockCover = useCallback(() => {
-    navigateTo(routes.inheritance.home.path);
-  }, [navigateTo]);
+  const renderContent = useMemo(
+    () => (
+      // if (isCantonAdded && isAutomaticApprovalEnabled) return null;
 
-  return (
-    <Container $bgColor="contentGradient" width="full">
-      <Container
-        direction="row"
-        $bgColor="featureBanner"
-        width="full"
-        justify="flex-start"
-        p={2}
-        $borderRadius={16}
-        gap={16}
-      >
-        <Image
-          src={cysyncLogoSmall}
-          alt="cypherock logo"
-          $width={24}
-          $height={30}
-        />
-        <Typography $fontSize={16} width="100%">
-          {lang.strings.inheritance.banner.title}
-        </Typography>
-        <Button variant="text" onClick={toCypherockCover}>
-          <Typography color="gold" $whiteSpace="nowrap">
-            {lang.strings.inheritance.banner.buttons.knowMore}
+      <Container $bgColor="contentGradient" width="full">
+        <Container
+          direction="row"
+          $bgColor="featureBanner"
+          width="full"
+          justify="flex-start"
+          p={2}
+          $borderRadius={16}
+          gap={16}
+        >
+          <Image src={cantonIcon} alt="canton logo" $width={30} $height={30} />
+          <Typography $fontSize={16} width="100%">
+            <LangDisplay
+              text={
+                isCantonAdded && !isAutomaticApprovalEnabled
+                  ? strings.enableAutomaticApprovals.title
+                  : strings.addCanton.title
+              }
+              $allowMarkdown
+            />
           </Typography>
-        </Button>
+          {isCantonAdded && !isAutomaticApprovalEnabled ? (
+            <Button variant="primary" onClick={enableAutomaticApprovals}>
+              {strings.enableAutomaticApprovals.button}
+            </Button>
+          ) : (
+            <Button variant="primary">
+              <a
+                href="https://cypherock.com"
+                target="_blank"
+                style={{
+                  textDecoration: 'none',
+                  color: 'black',
+                  whiteSpace: 'nowrap',
+                }}
+                rel="noreferrer"
+              >
+                {strings.addCanton.button}
+              </a>
+            </Button>
+          )}
+        </Container>
       </Container>
-    </Container>
+    ),
+    [isAutomaticApprovalEnabled, isCantonAdded],
   );
+
+  return renderContent;
 };
