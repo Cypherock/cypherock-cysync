290470fe1709dd6c24a70bac49eea72580b54d52
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2025-12-27 15:37:56 +0530

    fix: remove rewards button from canton account page

--- packages/cysync-core/src/pages/MainApp/Account/index.tsx
+++ packages/cysync-core/src/pages/MainApp/Account/index.tsx
@@ -22,7 +22,6 @@ import {
   openSendDialog,
 } from '~/actions';
 import { CoinIcon, Graph, TransactionTable } from '~/components';
-import { constants } from '~/constants';
 import { selectLanguage, useAppDispatch, useAppSelector } from '~/store';
 
 import { TokenTable } from './TokenTable';
@@ -102,25 +101,6 @@ export const AccountPage: FC = () => {
             >
               {lang.strings.buttons.receive}
             </Button>
-            {selectedAccount?.familyId === coinFamiliesMap.canton &&
-              selectedAccount.type !== AccountTypeMap.subAccount && (
-                <Button variant="primary">
-                  <a
-                    href={constants.inheritance.cantonLink}
-                    target="_blank"
-                    style={{
-                      textDecoration: 'none',
-                      color: 'black',
-                      whiteSpace: 'nowrap',
-                      fontSize: '12px',
-                      fontWeight: '500',
-                    }}
-                    rel="noreferrer"
-                  >
-                    {lang.strings.buttons.earnRewards}
-                  </a>
-                </Button>
-              )}
             {selectedAccount?.familyId === coinFamiliesMap.canton &&
               selectedAccount.type !== AccountTypeMap.subAccount &&
               !selectedAccount.extraData?.isTransferPreApprovalEnabled && (
